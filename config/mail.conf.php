9246a8d5875f80f32233da6057e9ecaa4b723842
Андрей <@strkns>
2013-02-17 10:22:04 +0400

    Изменение gitignore

---
+++ config/mail.conf.php
@@ -0,0 +1,10 @@
+<?php
+/* Mail settings */
+define('_MAIL_HOST', 'smtp.timeweb.ru');
+define('_MAIL_USER', 'noreply@culttourism.ru');
+define('_MAIL_PASS', 'Odc(h2m6u]Mh');
+define('_MAIL_FROMADDR', 'noreply@culttourism.ru');
+define('_MAIL_FROMNAME', 'Культурный туризм');
+define('_MAIL_TO', 'starkeen@gmail.com');
+define('_FEEDBACK_MAIL', 'starkeen@gmail.com');
+?>
