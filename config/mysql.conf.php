3b0af94a15e9a7573c2df3cdd6a075a24366ca8b
Андрей <@strkns>
2013-02-17 10:36:39 +0400

    Объединение конфигурационных файлов

--- config/mysql.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* MySQL settings */
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_BASENAME', 'cult');
-define('DB_PREFIX', 'cult');
-?>
