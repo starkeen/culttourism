848f68626463a26f86fd13357195c1f234dec6bf
Андрей <@strkns>
2013-02-17 11:31:13 +0400

    Перекомпиляция шаблонов

--- includes/class.mySmarty.php
+++ includes/class.mySmarty.php
@@ -13,7 +13,7 @@ class mySmarty extends Smarty {
 
         $this->caching = FALSE;
         $this->cache_lifetime = 3600;
-        $this->compile_check = FALSE;
+        $this->compile_check = true;
 
         $this->debugging = FALSE;
     }
