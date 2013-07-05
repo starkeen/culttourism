367274d9d06bfc61456cbc3ad6b59b049bf250b8
panisko <panisko@terraelectronica.ru>
2013-07-05 11:48:18 +0400

    Редирект логина

--- _admin/login.php
+++ _admin/login.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once('common.php');
 
 $smarty->assign('title', 'Авторизация в системе');
@@ -17,20 +18,19 @@ if (isset($_GET['out'])) {
 if (isset($_POST) && !empty($_POST)) {
     $login = cut_trash_word($_POST['login']);
     $pwd = cut_trash_text($_POST['pass']);
+    $retpath = (isset($_GET['r']) && $_GET['r'] != '') ? urldecode(cut_trash_text($_GET['r'])) : 'index.php';
 
     if ($ticket->checkPassword($login, $pwd)) {
         $_SESSION['auth'] = $ticket->key;
-        header('Location: index.php');
-    }
-    else {
-	$error = 'Авторизация не удалась. Попробуйте еще раз';
-        
+        header("Location: $retpath");
+    } else {
+        $error = 'Авторизация не удалась. Попробуйте еще раз';
     }
 }
 
 $smarty->assign('login', $login);
 $smarty->assign('error', $error);
-$smarty->assign('content', $smarty->fetch(_DIR_TEMPLATES.'/_admin/login.sm.html'));
-$smarty->display(_DIR_TEMPLATES.'/_admin/admpage.sm.html');
+$smarty->assign('content', $smarty->fetch(_DIR_TEMPLATES . '/_admin/login.sm.html'));
+$smarty->display(_DIR_TEMPLATES . '/_admin/admpage.sm.html');
 exit();
 ?>
