c878a5e3e3a0294ca36a234c81b82d8004258a30
starkeen <starkeen@gmail.com>
2014-04-20 21:02:02 +0400

    Модели доступа к данным типа Точки и Города

---
+++ models/class.Cities.php
@@ -0,0 +1,9 @@
+<?php
+
+class Cities extends Model {
+
+    public function __construct($db) {
+        parent::__construct($db);
+    }
+
+}
---
+++ models/class.Points.php
@@ -0,0 +1,9 @@
+<?php
+
+class Points extends Model {
+
+    public function __construct($db) {
+        parent::__construct($db);
+    }
+
+}
