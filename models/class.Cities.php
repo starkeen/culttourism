026b8cfa38d12056414c9dffc5c884df3812006b
panisko <panisko@Panisko.rbagroup.ru>
2014-04-21 14:58:39 +0400

    Основные методы в моделях

--- models/class.Cities.php
+++ models/class.Cities.php
@@ -2,8 +2,75 @@
 
 class Cities extends Model {
 
+    protected $_table_pk = 'pc_id';
+    protected $_table_order = 'pc_id';
+    protected $_table_active = 'pc_active';
+
     public function __construct($db) {
+        $this->_table_name = $db->getTableName('pagecity');
+        $this->_table_fields = array(
+            'pc_title',
+            'pc_text',
+            'pc_keywords',
+            'pc_description',
+            'pc_city_id',
+            'pc_url_id',
+            'pc_region_id',
+            'pc_country_id',
+            'pc_pagepath',
+            'pc_latitude',
+            'pc_longitude',
+            'pc_latlon_zoom',
+            'pc_cnt_shows',
+            'pc_rank',
+            'pc_inwheretext',
+            'pc_title_translit',
+            'pc_title_english',
+            'pc_title_synonym',
+            'pc_website',
+            'pc_lastup_date',
+            'pc_lastup_user',
+            'pc_add_date',
+            'pc_add_user',
+            'pc_active',
+        );
         parent::__construct($db);
+    }
+
+    public function updateByPk($id, $values = array(), $files = array()) {
+        if (isset($values['pc_latitude'])) {
+            $values['pc_latitude'] = floatval(str_replace(',', '.', trim($values['pc_latitude'])));
+            if ($values['pc_latitude'] == 0) {
+                unset($values['pc_latitude']);
+            }
+        }
+        if (isset($values['pc_longitude'])) {
+            $values['pc_longitude'] = floatval(str_replace(',', '.', trim($values['pc_longitude'])));
+            if ($values['pc_longitude'] == 0) {
+                unset($values['pc_longitude']);
+            }
+        }
+        parent::updateByPk($id, $values, $files);
+    }
+
+    public function insert($values = array(), $files = array()) {
+        if (isset($values['pc_latitude'])) {
+            $values['pc_latitude'] = floatval(str_replace(',', '.', trim($values['pc_latitude'])));
+            if ($values['pc_latitude'] == 0) {
+                unset($values['pc_latitude']);
+            }
+        }
+        if (isset($values['pc_longitude'])) {
+            $values['pc_longitude'] = floatval(str_replace(',', '.', trim($values['pc_longitude'])));
+            if ($values['pc_longitude'] == 0) {
+                unset($values['pc_longitude']);
+            }
+        }
+        parent::insert($values, $files);
+    }
+
+    public function deleteByPk($id) {
+        $this->updateByPk($id, array('pc_active' => 0,));
     }
 
 }
--- models/class.Points.php
+++ models/class.Points.php
@@ -2,8 +2,80 @@
 
 class Points extends Model {
 
+    protected $_table_pk = 'pt_id';
+    protected $_table_order = 'pt_id';
+    protected $_table_active = 'pt_active';
+
     public function __construct($db) {
+        $this->_table_name = $db->getTableName('pagepoints');
+        $this->_table_fields = array(
+            'pt_name',
+            'pt_description',
+            'pt_slugline',
+            'pt_latitude',
+            'pt_longitude',
+            'pt_latlon_zoom',
+            'pt_type_id',
+            'pt_create_date',
+            'pt_create_user',
+            'pt_lastup_date',
+            'pt_lastup_user',
+            'pt_city_id',
+            'pt_region_id',
+            'pt_country_id',
+            'pt_citypage_id',
+            'pt_cnt_shows',
+            'pt_rank',
+            'pt_website',
+            'pt_worktime',
+            'pt_adress',
+            'pt_phone',
+            'pt_email',
+            'pt_is_best',
+            'pt_active',
+        );
         parent::__construct($db);
+    }
+
+    public function getUnslug($limit = 10) {
+        $out = array();
+        return $out;
+    }
+
+    public function updateByPk($id, $values = array(), $files = array()) {
+        if (isset($values['pt_latitude'])) {
+            $values['pt_latitude'] = floatval(str_replace(',', '.', trim($values['pt_latitude'])));
+            if ($values['pt_latitude'] == 0) {
+                unset($values['pt_latitude']);
+            }
+        }
+        if (isset($values['pt_longitude'])) {
+            $values['pt_longitude'] = floatval(str_replace(',', '.', trim($values['pt_longitude'])));
+            if ($values['pt_longitude'] == 0) {
+                unset($values['pt_longitude']);
+            }
+        }
+        parent::updateByPk($id, $values, $files);
+    }
+
+    public function insert($values = array(), $files = array()) {
+        if (isset($values['pt_latitude'])) {
+            $values['pt_latitude'] = floatval(str_replace(',', '.', trim($values['pt_latitude'])));
+            if ($values['pt_latitude'] == 0) {
+                unset($values['pt_latitude']);
+            }
+        }
+        if (isset($values['pt_longitude'])) {
+            $values['pt_longitude'] = floatval(str_replace(',', '.', trim($values['pt_longitude'])));
+            if ($values['pt_longitude'] == 0) {
+                unset($values['pt_longitude']);
+            }
+        }
+        parent::insert($values, $files);
+    }
+
+    public function deleteByPk($id) {
+        $this->updateByPk($id, array('pt_active' => 0));
     }
 
 }
