cd626602440075f61d99bc335e98d9fcb855e442
panisko <panisko@Panisko.rbagroup.ru>
2014-05-15 14:39:49 +0400

    Автоматическая простановка slugline по точкам

--- cron/create_points_slugline.php
+++ cron/create_points_slugline.php
@@ -1,3 +1,37 @@
 <?php
 
+error_reporting(E_ALL);
+
 $pts = new Points($db);
+
+foreach ($pts->getUnslug() as $point) {
+    $name = trim($point['pt_name']);
+
+    $analogs = $pts->searchByName($point['pt_name']);
+    if ($point['tr_sight'] == 0 || count($analogs) > 1) {
+        $name = $point['pc_title'] . ' ' . $name;
+    }
+
+    $name_url = preg_replace('/[^a-z0-9-_]+/', '', strtolower(Helper::getTranslit($name, '_')));
+    $name_url = trim($name_url, '_-');
+
+    $concurents = $pts->searchSlugline($name_url);
+    if (count($concurents) > 0) {
+        $name_url = trim(strtolower(trim($point['pc_title_english'])) . '_' . $name_url);
+    }
+    $name_url = trim($name_url, '_-');
+
+    $concurents_else = $pts->searchSlugline($name_url);
+    if (count($concurents_else) > 0) {
+        $name_url .= '_' . count($concurents_else);
+    }
+    $name_url = trim($name_url, '_-');
+
+    $concurents_last = $pts->searchSlugline($name_url);
+    if (count($concurents_else) > 0) {
+        $name_url .= '_' . $point['pt_id'];
+    }
+    $name_url = trim($name_url, '_-');
+
+    $pts->updateByPk($point['pt_id'], array('pt_slugline' => $name_url));
+}
--- models/class.Points.php
+++ models/class.Points.php
@@ -38,8 +38,35 @@ class Points extends Model {
     }
 
     public function getUnslug($limit = 10) {
-        $out = array();
-        return $out;
+        $dbpc = $this->_db->getTableName('pagecity');
+        $dbrt = $this->_db->getTableName('ref_pointtypes');
+        $this->_db->sql = "SELECT pt_id, pt_name, pc_title, pc_title_english, tr_sight
+                            FROM $this->_table_name pt
+                                LEFT JOIN $dbpc pc ON pc.pc_id = pt.pt_citypage_id
+                                LEFT JOIN $dbrt rt ON rt.tp_id = pt.pt_type_id
+                            WHERE pt.pt_slugline = ''
+                            ORDER BY pt.pt_rank DESC
+                            LIMIT $limit";
+        $this->_db->exec();
+        return $this->_db->fetchAll();
+    }
+
+    public function searchSlugline($slugline) {
+        $this->_db->sql = "SELECT *
+                            FROM $this->_table_name pt
+                            WHERE TRIM(pt.pt_slugline) = TRIM('$slugline')
+                            ORDER BY pt.pt_rank DESC";
+        $this->_db->exec();
+        return $this->_db->fetchAll();
+    }
+
+    public function searchByName($name) {
+        $this->_db->sql = "SELECT *
+                            FROM $this->_table_name pt
+                            WHERE TRIM(pt.pt_name) = TRIM('$name')
+                            ORDER BY pt.pt_rank DESC";
+        $this->_db->exec();
+        return $this->_db->fetchAll();
     }
 
     public function updateByPk($id, $values = array(), $files = array()) {
