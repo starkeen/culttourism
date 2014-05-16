14a45415c06edbf1636af92d671027db34119770
panisko <panisko@Panisko.rbagroup.ru>
2014-05-16 12:06:06 +0400

    Перенос формирования slugline в модель и проверка готовых строк

--- cron/create_points_slugline.php
+++ cron/create_points_slugline.php
@@ -5,33 +5,10 @@ error_reporting(E_ALL);
 $pts = new Points($db);
 
 foreach ($pts->getUnslug() as $point) {
-    $name = trim($point['pt_name']);
+    $pts->createSluglineById($point['pt_id']);
+}
 
-    $analogs = $pts->searchByName($point['pt_name']);
-    if ($point['tr_sight'] == 0 || count($analogs) > 1) {
-        $name = $point['pc_title'] . ' ' . $name;
-    }
-
-    $name_url = preg_replace('/[^a-z0-9-_]+/', '', strtolower(Helper::getTranslit($name, '_')));
-    $name_url = trim($name_url, '_-');
-
-    $concurents = $pts->searchSlugline($name_url);
-    if (count($concurents) > 0) {
-        $name_url = trim(strtolower(trim($point['pc_title_english'])) . '_' . $name_url);
-    }
-    $name_url = trim($name_url, '_-');
-
-    $concurents_else = $pts->searchSlugline($name_url);
-    if (count($concurents_else) > 0) {
-        $name_url .= '_' . count($concurents_else);
-    }
-    $name_url = trim($name_url, '_-');
-
-    $concurents_last = $pts->searchSlugline($name_url);
-    if (count($concurents_else) > 0) {
-        $name_url .= '_' . $point['pt_id'];
-    }
-    $name_url = trim($name_url, '_-');
-
-    $pts->updateByPk($point['pt_id'], array('pt_slugline' => $name_url));
+$check = $pts->checkSluglines();
+if (!$check['state']) {
+    echo "Errors in points //" . implode("\n", $check['errors']);
 }
--- models/class.Points.php
+++ models/class.Points.php
@@ -69,6 +69,74 @@ class Points extends Model {
         return $this->_db->fetchAll();
     }
 
+    public function createSluglineById($id) {
+        $point = $this->getItemByPk($id);
+
+        $name = trim($point['pt_name']);
+
+        $analogs = $this->searchByName($point['pt_name']);
+        if ($point['tr_sight'] == 0 || count($analogs) > 1) {
+            $name = $point['pc_title'] . ' ' . $name;
+        }
+        $name_url = trim(preg_replace('/[^a-z0-9-_]+/', '', strtolower(Helper::getTranslit($name, '_'))), '_-');
+
+        $concurents = $this->searchSlugline($name_url);
+        if (count($concurents) > 0) {
+            $name_url = trim(trim(strtolower(trim($point['pc_title_english'])) . '_' . $name_url), '_-');
+        }
+
+        $concurents_else = $this->searchSlugline($name_url);
+        if (count($concurents_else) > 0) {
+            $name_url = trim($name_url . '_' . count($concurents_else), '_-');
+        }
+
+        $concurents_last = $this->searchSlugline($name_url);
+        if (count($concurents_last) > 0) {
+            $name_url = trim($name_url . '_' . $point['pt_id'], '_-');
+        }
+
+        $this->updateByPk($point['pt_id'], array('pt_slugline' => $name_url));
+    }
+
+    public function checkSluglines() {
+        $out = array(
+            'state' => true,
+            'errors' => array(),
+            'doubles' => array(),
+        );
+        $this->_db->sql = "SELECT pt_id, pt_name, pt.pt_slugline, count(*) AS cnt
+                            FROM $this->_table_name pt
+                            WHERE pt.pt_slugline != ''
+                            GROUP BY pt.pt_slugline
+                            HAVING cnt > 1
+                            ORDER BY pt.pt_rank DESC";
+        $this->_db->exec();
+        $out['doubles'] = $this->_db->fetchAll();
+        if (count($out['doubles']) > 0) {
+            $out['state'] = false;
+            foreach ($out['doubles'] as $i => $double) {
+                $out['doubles'][$i]['objects'] = $this->searchSlugline($double['pt_slugline']);
+                foreach ($out['doubles'][$i]['objects'] as $obj) {
+                    $out['errors'][] = "Дублирование Slugline '{$double['pt_slugline']}': {$obj['pt_id']} / {$obj['pt_name']}";
+                }
+            }
+        }
+        return $out;
+    }
+
+    public function getItemByPk($id) {
+        $id = intval($id);
+        $dbpc = $this->_db->getTableName('pagecity');
+        $dbrt = $this->_db->getTableName('ref_pointtypes');
+        $this->_db->sql = "SELECT *
+                            FROM $this->_table_name pt
+                                LEFT JOIN $dbpc pc ON pc.pc_id = pt.pt_citypage_id
+                                LEFT JOIN $dbrt rt ON rt.tp_id = pt.pt_type_id
+                            WHERE $this->_table_pk = '$id'";
+        $this->_db->exec();
+        return $this->_db->fetch();
+    }
+
     public function updateByPk($id, $values = array(), $files = array()) {
         if (isset($values['pt_latitude'])) {
             $values['pt_latitude'] = floatval(str_replace(',', '.', trim($values['pt_latitude'])));
