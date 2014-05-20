4372b996e6f8ee3af3dd58e3ed799874f8fb1bf0
panisko <panisko@Panisko.rbagroup.ru>
2014-05-20 12:31:05 +0400

    Страница региона переведена на использование универсальной модели

--- includes/class.Page.php
+++ includes/class.Page.php
@@ -88,11 +88,7 @@ class Page extends PageCommon {
 
         //------------------  s t a t i s t i c s  ------------------------
         $sp = new Statpoints($this->db);
-        $sp->insert(array(
-            'sp_pagepoint_id' => $object['pt_id'],
-            'sp_date' => date('Y-m-d H:i:s'),
-            'sp_hash' => $this->getUserHash(),
-        ));
+        $sp->add($object['pt_id'], $this->getUserHash());
 
         $this->isCounters = 1;
         $this->getCounters();
@@ -205,186 +201,41 @@ class Page extends PageCommon {
     }
 
     private function getPageCity($db, $smarty, $url) {
-        $dburl = $db->getTableName('region_url');
-        $dbpc = $db->getTableName('pagecity');
-        $dbpt = $db->getTableName('pagepoints');
-        $dbrp = $db->getTableName('ref_pointtypes');
-        $dbcd = $db->getTableName('city_data');
-        $dbcf = $db->getTableName('city_fields');
-
         if (array_pop(explode('/', $url)) == 'index.html') {
             header("HTTP/1.1 301 Moved Permanently");
             header("Location: " . str_replace("index.html", '', $url));
             exit();
         }
 
-        $url = mysql_real_escape_string($url);
+        $pcs = new Cities($this->db);
+        $pts = new Points($this->db);
+        $row = $pcs->getCityByUrl($url);
 
-        $db->sql = "SELECT city.*
-                    FROM $dburl url
-                    LEFT JOIN $dbpc city ON city.pc_id = url.citypage
-                    WHERE url.url = '$url'";
-
-        $res = $db->exec();
-        if (!$res) {
-            return FALSE;
-        }
-        if ($row = $db->fetch()) {
+        if (!empty($row)) {
             $row['pc_zoom'] = ($row['pc_latlon_zoom']) ? $row['pc_latlon_zoom'] : 12;
+            $this->lastedit_timestamp = $row['last_update'];
 
             //--------------------  c a n o n i c a l  ------------------------
-            $db->sql = "SELECT url FROM $dburl WHERE uid = '{$row['pc_url_id']}'";
-            $db->exec();
-            $canonical_url = $db->fetch();
-            if ($canonical_url['url'] != '') {
-                $this->canonical = $canonical_url['url'] . '/';
-                if ($canonical_url['url'] != $url) {
-                    header("HTTP/1.1 301 Moved Permanently");
-                    header("Location: {$canonical_url['url']}/");
-                    exit();
-                }
+            $this->canonical = $row['url_canonical'];
+            if ($this->canonical != ($url . '/')) {
+                header("HTTP/1.1 301 Moved Permanently");
+                header("Location: $this->canonical");
+                exit();
             }
 
+            $points_data = $pts->getPointsByCity($row['pc_id'], $this->checkEdit());
 
-            //----------------------  в н у т р и  ------------------------
-            $row['region_in'] = array();
-            if ($row['pc_region_id'] > 0 && $row['pc_city_id'] == 0) {
-                $db->sql = "SELECT pc.pc_title, url.url, pc.pc_inwheretext,
-                                    UNIX_TIMESTAMP(pc.pc_add_date) AS last_update
-                            FROM $dbpc pc
-                            LEFT JOIN $dburl url ON url.uid = pc.pc_url_id
-                            WHERE pc.pc_region_id = '{$row['pc_region_id']}'
-                            AND pc.pc_city_id != 0
-                            ORDER BY pc.pc_rank DESC, pc.pc_title";
-                $db->exec();
-                while ($subcity = $db->fetch()) {
-                    $row['region_in'][] = array('title' => $subcity['pc_title'], 'url' => $subcity['url'], 'where' => $subcity['pc_inwheretext']);
-                    if ($subcity['last_update'] > $this->lastedit_timestamp) {
-                        $this->lastedit_timestamp = $subcity['last_update'];
-                    }
-                }
+            if ($points_data['last_update'] > $this->lastedit_timestamp) {
+                $this->lastedit_timestamp = $points_data['last_update'];
             }
-            //----------------------  р я д о м  ------------------------
-            $row['region_near'] = array();
-            if ($row['pc_region_id'] > 0 && $row['pc_city_id'] > 0) {
-                $db->sql = "SELECT pc.pc_title, url.url, pc.pc_inwheretext,
-                                    ROUND(1000 * (ABS(pc.pc_latitude - {$row['pc_latitude']}) + ABS(pc.pc_longitude - {$row['pc_longitude']}))) AS delta_sum,
-                                    UNIX_TIMESTAMP(pc.pc_add_date) AS last_update
-                            FROM $dbpc pc
-                            LEFT JOIN $dburl url ON url.uid = pc.pc_url_id
-                            WHERE pc.pc_city_id != 0
-                            AND pc.pc_title != '{$row['pc_title']}'
-                            AND pc.pc_latitude > 0 AND pc.pc_longitude > 0
-                            HAVING delta_sum < 5000
-                            ORDER BY delta_sum
-                            LIMIT 10";
-                //$db->showSQL();
-                $db->exec();
-                while ($subcity = $db->fetch()) {
-                    $row['region_near'][] = array(
-                        'title' => $subcity['pc_title'],
-                        'url' => $subcity['url'],
-                        'where' => $subcity['pc_inwheretext'],
-                    );
-                    if ($subcity['last_update'] > $this->lastedit_timestamp) {
-                        $this->lastedit_timestamp = $subcity['last_update'];
-                    }
-                }
+            if ($this->checkEdit()) {
+                $this->lastedit_timestamp = 0;
             }
 
-            //-----------------------  м е т а  -------------------------
-            $db->sql = "SELECT cf_title, cd_value
-                        FROM $dbcd cd
-                            LEFT JOIN $dbcf cf ON cf.cf_id = cd.cd_cf_id
-                        WHERE cd.cd_pc_id = '{$row['pc_id']}'
-                            AND cd.cd_value != ''
-                            AND cf.cf_active = 1
-                        ORDER BY cf_order";
-            $db->exec();
-            $row['metas'] = array();
-            while ($mrow = $db->fetch()) {
-                $row['metas'][] = $mrow;
-            }
-
-            //----------------------  т о ч к и  ------------------------
-            $db->sql = "SELECT pt.*, pt.pt_id, pt.pt_name, pt.pt_type_id, pt.pt_description,
-                                pt_latitude, pt_longitude, pt_latlon_zoom,
-                                pt.pt_active,
-                                rp.tp_name, rp.tp_icon, rp.tp_short, rp.tr_sight, rp.tp_icon,
-                                UNIX_TIMESTAMP(pt.pt_lastup_date) AS last_update
-                        FROM $dbpt pt
-                        LEFT JOIN $dbrp rp ON rp.tp_id = pt.pt_type_id
-                        WHERE pt.pt_citypage_id = '{$row['pc_id']}'\n";
-            if (!$this->checkEdit()) {
-                $db->sql .= "AND pt.pt_active = 1\n";
-            }
-            $db->sql .= "ORDER BY pt.pt_active DESC, rp.tr_sight desc, pt.pt_rank desc, rp.tr_order, pt.pt_name";
-            //$db->showSQL();
-            $db->exec();
-            $points = array();
-            $pnts_sight = array();
-            $pnts_service = array();
-            $alltypes = array();
-            while ($point = $db->fetch()) {
-                $alltypes[$point['tr_sight']][$point['pt_type_id']] = array('short' => $point['tp_short'], 'full' => $point['tp_name'], 'icon' => $point['tp_icon']);
-
-                $short_lenght = 300;
-                $point['short'] = html_entity_decode(strip_tags($point['pt_description']), ENT_QUOTES, 'utf-8');
-                $short_end = @mb_strpos($point['short'], '.', $short_lenght, 'utf-8');
-                if (mb_strlen($point['short']) >= $short_lenght && $short_end) {
-                    $point['short'] = mb_substr($point['short'], 0, $short_end, 'utf-8') . '&hellip;';
-                }
-
-                $point['pt_name'] = htmlentities($point['pt_name'], ENT_QUOTES, 'UTF-8', false);
-
-                $point_lat = $point['pt_latitude'];
-                $point_lon = $point['pt_longitude'];
-                if ($point_lat && $point_lon) {
-                    $point_lat_short = mb_substr($point_lat, 0, 8);
-                    $point_lon_short = mb_substr($point_lon, 0, 8);
-                    if ($point_lat >= 0) {
-                        $point_lat_w = "N$point_lat_short";
-                    } else {
-                        $point_lat_w = "S$point_lat_short";
-                    }
-                    if ($point_lon >= 0) {
-                        $point_lon_w = "E$point_lon_short";
-                    } else {
-                        $point_lon_w = "W$point_lon_short";
-                    }
-                    $point['gps_dec'] = "$point_lat_w $point_lon_w";
-                } else {
-                    $point['gps_dec'] = null;
-                }
-                $points[] = $point;
-                if ($point['tr_sight'] == 1) {
-                    $pnts_sight[] = $point;
-                } else {
-                    $pnts_service[] = $point;
-                }
-
-                if ($point['last_update'] > $this->lastedit_timestamp) {
-                    $this->lastedit_timestamp = $point['last_update'];
-                }
-                if ($this->checkEdit()) {
-                    $this->lastedit_timestamp = 0;
-                }
-            }
             $this->lastedit = gmdate('D, d M Y H:i:s', $this->lastedit_timestamp) . ' GMT';
 
-            $smarty->assign('city', $row);
-            $smarty->assign('points', $points);
-            $smarty->assign('points_sight', $pnts_sight);
-            $smarty->assign('points_servo', $pnts_service);
-            $smarty->assign('page_url', $this->basepath);
-
-            $smarty->assign('types_select', $alltypes);
-
-            $dbsc = $db->getTableName('statcity');
-            $hash = $this->getUserHash();
-            $db->sql = "INSERT INTO $dbsc (sc_citypage_id, sc_date, sc_hash) VALUES ('{$row['pc_id']}', now(), '$hash')
-                        ON DUPLICATE KEY UPDATE sc_date = now()";
-            $db->exec();
+            $sc = new Statcity($this->db);
+            $sc->add($row['pc_id'], $this->getUserHash());
 
             $this->addTitle($row['pc_title'] . ': достопримечательности');
             if ($row['pc_description']) {
@@ -406,10 +257,17 @@ class Page extends PageCommon {
             $this->isCounters = 1;
             $this->getCounters();
 
+            $this->smarty->assign('city', $row);
+            $this->smarty->assign('points', $points_data['points']);
+            $this->smarty->assign('points_sight', $points_data['points_sight']);
+            $this->smarty->assign('points_servo', $points_data['points_service']);
+            $this->smarty->assign('page_url', $this->basepath);
+            $this->smarty->assign('types_select', $points_data['types']);
+
             if ($this->checkEdit()) {
-                return $smarty->fetch(_DIR_TEMPLATES . '/_pages/pagecity.edit.sm.html');
+                return $this->smarty->fetch(_DIR_TEMPLATES . '/_pages/pagecity.edit.sm.html');
             } else {
-                return $smarty->fetch(_DIR_TEMPLATES . '/_pages/pagecity.show.sm.html');
+                return $this->smarty->fetch(_DIR_TEMPLATES . '/_pages/pagecity.show.sm.html');
             }
         } else {
             return FALSE;
