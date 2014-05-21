124418679e5a8044f5b7c26c5ae07782dafdce14
panisko <panisko@Panisko.rbagroup.ru>
2014-05-21 11:33:40 +0400

    Перенос API большой карты в модуль карт

--- js/map.js
+++ js/map.js
@@ -24,8 +24,8 @@ ymaps.ready(function() {
             });
         }
         var params = [
-        'center=' + longitude + ',' + latitude,
-        'zoom=' + zoom
+            'center=' + longitude + ',' + latitude,
+            'zoom=' + zoom
         ];
         window.location.hash = params.join('&');
     }
@@ -35,57 +35,57 @@ ymaps.ready(function() {
         behaviors: ['default', 'scrollZoom'],
         type: 'yandex#publicMap'
     }, {
-        minZoom:4
+        minZoom: 4
     });
     map.controls.add("zoomControl").add("mapTools").add(new ymaps.control.TypeSelector(["yandex#map", "yandex#satellite", "yandex#hybrid", "yandex#publicMap"]));
-                
+
     var cluster = new ymaps.Clusterer({
         margin: [20],
         clusterIcons: [{
-            href: '/img/points/map/cluster.png',
-            size: [40, 40],
-            offset: [-20, -20]
-        }],
+                href: '/img/points/map/cluster.png',
+                size: [40, 40],
+                offset: [-20, -20]
+            }],
         clusterNumbers: [100],
         gridSize: 50
     });
-    
+
     var bounds = map.getBounds();
     var boundsparams = [
-    'lln='+bounds[0][0],
-    'llt='+bounds[0][1],
-    'rln='+bounds[1][0],
-    'rlt='+bounds[1][1],
+        'lln=' + bounds[0][0],
+        'llt=' + bounds[0][1],
+        'rln=' + bounds[1][0],
+        'rlt=' + bounds[1][1],
     ];
-                
-    ymaps.geoXml.load("http://culttourism.ru/ajax/YMapsML/getcommonmap/?"+boundsparams.join('&')).then(function (res) {
+
+    ymaps.geoXml.load("http://culttourism.ru/map/common/?" + boundsparams.join('&')).then(function(res) {
         if (res.mapState && !hashCenter)
             res.mapState.applyToMap(map);
         var arr = [];
-        res.geoObjects.each(function (obj) {
+        res.geoObjects.each(function(obj) {
             arr.push(obj);
         });
         $('#mapdata_points').text(arr.length);
         cluster.add(arr);
         map.geoObjects.add(cluster);
     });
-    
-    map.events.add(['boundschange', 'typechange'], function () {
+
+    map.events.add(['boundschange', 'typechange'], function() {
         var params = [
-        'center=' + map.getCenter(),
-        'zoom=' + map.getZoom()
+            'center=' + map.getCenter(),
+            'zoom=' + map.getZoom()
         ];
         window.location.hash = params.join('&');
         bounds = map.getBounds();
         boundsparams = [
-        'lln='+bounds[0][0],
-        'llt='+bounds[0][1],
-        'rln='+bounds[1][0],
-        'rlt='+bounds[1][1],
+            'lln=' + bounds[0][0],
+            'llt=' + bounds[0][1],
+            'rln=' + bounds[1][0],
+            'rlt=' + bounds[1][1],
         ];
-        ymaps.geoXml.load("http://culttourism.ru/ajax/YMapsML/getcommonmap/?"+boundsparams.join('&')).then(function (res) {
+        ymaps.geoXml.load("http://culttourism.ru/map/common/?" + boundsparams.join('&')).then(function(res) {
             var arr = [];
-            res.geoObjects.each(function (obj) {
+            res.geoObjects.each(function(obj) {
                 arr.push(obj);
             });
             $('#mapdata_points').text(arr.length);
@@ -95,11 +95,11 @@ ymaps.ready(function() {
             map.geoObjects.add(cluster);
         });
     });
-    
+
     var locationparams = [ymaps.geolocation.country, ymaps.geolocation.region, ymaps.geolocation.city];
     $('#mapdata_location').text(locationparams.join(', '));
 });
-function getHashParam (name, location) {
+function getHashParam(name, location) {
     location = location || window.location.hash;
     var res = location.match(new RegExp('[#&]' + name + '=([^&]*)', 'i'));
     return (res && res[1] ? res[1] : false);
--- modules/map/map.php
+++ modules/map/map.php
@@ -19,10 +19,105 @@ class Page extends PageCommon {
             $this->content = $smarty->fetch(_DIR_TEMPLATES . '/map/map.sm.html');
             return true;
         }
+        //====================  M A P   E N T R Y  ============================
+        elseif ($page_id == 'common') {
+            $this->auth->setService('map');
+            $this->getYMapsMLCommon($_GET);
+        }
         //==========================  E X I T  ================================
         else {
             $this->getError('404');
         }
+    }
+
+    private function getYMapsMLCommon($get) {
+        $dbpr = $this->db->getTableName('ref_pointtypes');
+        $dbpp = $this->db->getTableName('pagepoints');
+        $dbpc = $this->db->getTableName('pagecity');
+        $dbru = $this->db->getTableName('region_url');
+
+        $ptypes = array();
+        $bounds = array(
+            'max_lat' => 55.9864578247, 'max_lon' => 37.9002265930,
+            'min_lat' => 55.4144554138, 'min_lon' => 37.1716384888,
+            'center_lat' => null, 'center_lon' => null,
+            'delta_lat' => 0.1, 'delta_lon' => 0.3,
+        );
+        $points = array();
+
+        $this->db->sql = "SELECT * FROM $dbpr";
+        $this->db->exec();
+        while ($rpt = $this->db->fetch()) {
+            $ptypes[] = $rpt;
+        }
+
+        if (!isset($get['center']) && isset($get['clt']) && isset($get['cln']) && !isset($get['llt']) && !isset($get['lln']) && !isset($get['rlt']) && !isset($get['rln'])) {
+            //---------- по координатам центра (раздельно)
+            $bounds['center_lat'] = cut_trash_float($get['clt']);
+            $bounds['center_lon'] = cut_trash_float($get['cln']);
+            $bounds['max_lat'] = $bounds['center_lat'] + $bounds['delta_lat'];
+            $bounds['max_lon'] = $bounds['center_lon'] + $bounds['delta_lon'];
+            $bounds['min_lat'] = $bounds['center_lat'] - $bounds['delta_lat'];
+            $bounds['min_lon'] = $bounds['center_lon'] - $bounds['delta_lon'];
+        } elseif (isset($get['center']) && !isset($get['clt']) && !isset($get['cln']) && !isset($get['llt']) && !isset($get['lln']) && !isset($get['rlt']) && !isset($get['rln'])) {
+            //---------- по координатам центра (в одном)
+            $center = explode(',', $get['center']);
+            $bounds['center_lat'] = cut_trash_float($center[1]);
+            $bounds['center_lon'] = cut_trash_float($center[0]);
+            $bounds['max_lat'] = $bounds['center_lat'] + $bounds['delta_lat'];
+            $bounds['max_lon'] = $bounds['center_lon'] + $bounds['delta_lon'];
+            $bounds['min_lat'] = $bounds['center_lat'] - $bounds['delta_lat'];
+            $bounds['min_lon'] = $bounds['center_lon'] - $bounds['delta_lon'];
+        } elseif (!isset($get['center']) && isset($get['llt']) && isset($get['lln']) && isset($get['rlt']) && isset($get['rln']) && !isset($get['clt']) && !isset($get['cln'])) {
+            //---------- по координатам левого и правого угла
+            $bounds['max_lat'] = cut_trash_float($get['rlt']);
+            $bounds['max_lon'] = cut_trash_float($get['rln']);
+            $bounds['min_lat'] = cut_trash_float($get['llt']);
+            $bounds['min_lon'] = cut_trash_float($get['lln']);
+            $bounds['delta_lat'] = $bounds['max_lat'] - $bounds['min_lat'];
+            $bounds['delta_lon'] = $bounds['max_lon'] - $bounds['min_lon'];
+            $bounds['center_lat'] = $bounds['min_lat'] + $bounds['delta_lat'];
+            $bounds['center_lon'] = $bounds['min_lon'] + $bounds['delta_lon'];
+        } else {
+            //---------- по умолчанию берем Москву
+            $bounds['delta_lat'] = $bounds['max_lat'] - $bounds['min_lat'];
+            $bounds['delta_lon'] = $bounds['max_lon'] - $bounds['min_lon'];
+            $bounds['center_lat'] = $bounds['min_lat'] + $bounds['delta_lat'];
+            $bounds['center_lon'] = $bounds['min_lon'] + $bounds['delta_lon'];
+        }
+
+        $this->db->sql = "SELECT pp.*,
+                                CONCAT('" . _URL_ROOT . "', ru.url, '/') AS cityurl,
+                                #CONCAT('" . _URL_ROOT . "', ru.url, '/', pp.pt_slugline, '.html') AS objurl
+                                CONCAT('" . _URL_ROOT . "', ru.url, '/object', pp.pt_id, '.html') AS objurl
+                            FROM $dbpp AS pp
+                                LEFT JOIN $dbpr pt ON pt.tp_id = pp.pt_type_id
+                                LEFT JOIN $dbpc pc ON pc.pc_id = pp.pt_citypage_id
+                                    LEFT JOIN $dbru ru ON ru.uid = pc.pc_url_id
+                            WHERE pp.pt_active = 1
+                                AND pp.pt_latitude BETWEEN '{$bounds['min_lat']}' AND '{$bounds['max_lat']}'
+                                AND pp.pt_longitude BETWEEN '{$bounds['min_lon']}' AND '{$bounds['max_lon']}'
+                            ORDER BY pt.tr_order DESC, pp.pt_rank
+                            LIMIT 300";
+        $this->db->exec();
+        while ($pt = $this->db->fetch()) {
+            $pt['pt_description'] = strip_tags($pt['pt_description']);
+            $pt['pt_description'] = html_entity_decode($pt['pt_description'], ENT_QUOTES, 'UTF-8');
+            $short_end = @mb_strpos($pt['pt_description'], ' ', 50, 'utf-8');
+            $pt['pt_short'] = trim(mb_substr($pt['pt_description'], 0, $short_end, 'utf-8'), "\x00..\x1F,.-");
+            $pt['pt_website'] = htmlspecialchars($pt['pt_website'], ENT_QUOTES);
+            $points[] = $pt;
+        }
+
+        $this->smarty->assign('ptypes', $ptypes);
+        $this->smarty->assign('bounds', $bounds);
+        $this->smarty->assign('points', $points);
+
+        header("Content-type: application/xml");
+        header("Cache-Control: no-store, no-cache, must-revalidate");
+        header("Expires: " . date("r"));
+        echo $this->smarty->fetch(_DIR_TEMPLATES . '/_XML/YMapsML3.sm.xml');
+        exit();
     }
 
     public static function getInstance($db, $mod) {
