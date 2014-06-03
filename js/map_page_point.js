5031c3811327b5ffc77d299529e9fd76ec4b4b09
panisko <panisko@Panisko.rbagroup.ru>
2014-06-03 17:12:10 +0400

    Использование единого скрипта вывода карты для точек

--- js/map_page_point.js
+++ js/map_page_point.js
@@ -3,20 +3,39 @@ ymaps.ready(function() {
         center: [$('#mapobj_pt_longitude').val(), $('#mapobj_pt_latitude').val()],
         zoom: $('#mapobj_pt_zoom').val(),
         behaviors: ['default', 'scrollZoom']
-    }),
-            // Создаем метку и задаем изображение для ее иконки
-            myPlacemark = new ymaps.Placemark([$('#mapobj_pt_longitude').val(), $('#mapobj_pt_latitude').val()], {
-                balloonContent: $('#mapobj_pt_name').val()
-            }, {
-                iconImageHref: '/img/points/xmap/' + $('#mapobj_pt_type_pic').val(), // картинка иконки
-                iconImageSize: [55, 55], // размеры картинки
-                iconImageOffset: [-27, -55] // смещение картинки
-            });
+    });
     myMap.controls.add("zoomControl").add("mapTools").add(new ymaps.control.TypeSelector(["yandex#map", "yandex#hybrid"]));
     $.getScript('/js/nmap-autoswitcher/nmap-autoswitcher.js', function() {
         var autoSwitcher = new AutoSwitcher();
         autoSwitcher.addToMap(myMap);
     });
-    // Добавление метки на карту
-    myMap.geoObjects.add(myPlacemark);
+
+    var cluster = new ymaps.Clusterer({
+        margin: [20],
+        clusterIcons: [{
+                href: '/img/points/map/cluster.png',
+                size: [40, 40],
+                offset: [-20, -20]
+            }],
+        clusterNumbers: [100],
+        gridSize: 50
+    });
+    var bounds = myMap.getBounds();
+    var boundsparams = [
+        'lln=' + bounds[0][0],
+        'llt=' + bounds[0][1],
+        'rln=' + bounds[1][0],
+        'rlt=' + bounds[1][1]
+    ];
+    ymaps.geoXml.load("http://culttourism.ru/map/common/?oid=" + $("#mapobj_pt_id").val() + "&" + boundsparams.join('&')).then(function(res) {
+        if (res.mapState) {
+            res.mapState.applyToMap(myMap);
+        }
+        var arr = [];
+        res.geoObjects.each(function(obj) {
+            arr.push(obj);
+        });
+        cluster.add(arr);
+        myMap.geoObjects.add(cluster);
+    });
 });
