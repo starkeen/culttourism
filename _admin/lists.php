1f89ce079a70e36fe660543f032864b6c3e9431c
panisko <panisko@Panisko.rbagroup.ru>
2014-06-05 17:03:38 +0400

    Редактирование описательной части списка

--- _admin/lists.php
+++ _admin/lists.php
@@ -5,7 +5,26 @@ include (_DIR_INCLUDES . '/class.Pager.php');
 
 $smarty->assign('title', 'Списки объектов');
 
-$smarty->assign('content', $smarty->fetch(_DIR_TEMPLATES . '/_admin/lists.list.sm.html'));
+$lst = new Lists($db);
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $list = $lst->getItemByPk($id);
+
+    if (isset($_POST) && !empty($_POST)) {
+        $res = $lst->updateByPk($id, $_POST);
+        if ($res) {
+            header("Location: lists.php");
+            exit();
+        }
+    }
+
+    $smarty->assign('list', $list);
+    $smarty->assign('content', $smarty->fetch(_DIR_TEMPLATES . '/_admin/lists.item.sm.html'));
+} else {
+    $smarty->assign('lists', $lst->getAll());
+    $smarty->assign('content', $smarty->fetch(_DIR_TEMPLATES . '/_admin/lists.list.sm.html'));
+}
 
 $smarty->display(_DIR_TEMPLATES . '/_admin/admpage.sm.html');
 exit();
--- models/class.Lists.php
+++ models/class.Lists.php
@@ -20,6 +20,20 @@ class Lists extends Model {
         parent::__construct($db);
     }
 
+    public function getAll() {
+        $dbi = $this->_db->getTableName('lists_items');
+        $this->_db->sql = "SELECT ls.*,
+                                (SELECT COUNT(*) FROM $dbi WHERE li_ls_id = ls.ls_id) AS cnt,
+                                CHAR_LENGTH(TRIM(ls_description)) AS len_descr,
+                                CHAR_LENGTH(TRIM(ls_text)) AS len_text
+                            FROM $this->_table_name ls\n";
+        if ($this->_table_order) {
+            $this->_db->sql .= "ORDER BY $this->_table_order ASC\n";
+        }
+        $this->_db->exec();
+        return $this->_db->fetchAll();
+    }
+
     public function deleteByPk($id) {
         $this->updateByPk($id, array($this->_table_active => 0));
     }
