df3794af2e1869f463d608db031619a0841248c8
starkeen <starkeen@gmail.com>
2014-10-06 22:13:54 +0400

    Оптимизация запроса

--- cron/base_cleaner.php
+++ cron/base_cleaner.php
@@ -1,22 +1,23 @@
 <?php
 
 $dbau = $db->getTableName('authorizations');
-$dbni = $db->getTableName('news_items');
-$dbla = $db->getTableName('log_actions');
-$dble = $db->getTableName('log_errors');
 
-$db->sql = "DELETE FROM $dbau WHERE (au_date_expire < now() OR au_service = 'ajax')";
+$tables = array(
+    array($db->getTableName('authorizations'), 'au_date_expire', 7),
+    array($db->getTableName('news_items'), 'ni_pubdate', 3,),
+    array($db->getTableName('log_actions'), 'la_date', 60),
+    array($db->getTableName('log_errors'), 'le_date', 30),
+);
+
+$db->sql = "DELETE FROM $dbau WHERE au_service = 'ajax')";
 $db->exec();
 
-$db->sql = "DELETE FROM $dbni WHERE DATEDIFF(now(), ni_pubdate) >= 3";
-$db->exec();
-
-$db->sql = "DELETE FROM $dbla WHERE DATEDIFF(now(), la_date) > 60";
-$db->exec();
-
-$db->sql = "DELETE FROM $dble WHERE DATEDIFF(now(), le_date) > 30";
-$db->exec();
-
-$db->sql = "OPTIMIZE TABLE $dbau, $dbni, $dbla, $dble";
-$db->exec();
+foreach ($tables as $table) {
+    $db->sql = "DELETE FROM {$table[0]}
+                WHERE {$table[1]} < SUBDATE(NOW(), INTERVAL {$table[2]} DAY)"
+            . (isset($table[3]) ? " AND {$table[3]}" : '');
+    $db->exec();
+    $db->sql = "OPTIMIZE TABLE {$table[0]}";
+    $db->exec();
+}
 ?>
