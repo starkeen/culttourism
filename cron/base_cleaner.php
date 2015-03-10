d6ff6ab95a4cbf22d3f84b254b1c76f62aacf7b4
starkeen <starkeen@gmail.com>
2015-03-10 23:02:55 +0400

    Уточнение логики оптимизации БД

--- cron/base_cleaner.php
+++ cron/base_cleaner.php
@@ -3,21 +3,22 @@
 $dbau = $db->getTableName('authorizations');
 
 $tables = array(
-    array($db->getTableName('authorizations'), 'au_date_expire', 7),
+    array($db->getTableName('authorizations'), 'au_date_expire', 1),
     array($db->getTableName('news_items'), 'ni_pubdate', 3,),
-    array($db->getTableName('log_actions'), 'la_date', 60),
-    array($db->getTableName('log_errors'), 'le_date', 30),
+    array($db->getTableName('log_actions'), 'la_date', 60,),
+    array($db->getTableName('log_errors'), 'le_date', 30,),
 );
 
 $db->sql = "DELETE FROM $dbau WHERE au_service IN ('ajax', 'map'))";
 $db->exec();
 
-foreach ($tables as $table) {
+foreach ($tables as $i => $table) {
     $db->sql = "DELETE FROM {$table[0]}
                 WHERE {$table[1]} < SUBDATE(NOW(), INTERVAL {$table[2]} DAY)"
             . (isset($table[3]) ? " AND {$table[3]}" : '');
     $db->exec();
-    $db->sql = "OPTIMIZE TABLE {$table[0]}";
-    $db->exec();
+    if ($i != 0) {
+        $db->sql = "OPTIMIZE TABLE {$table[0]}";
+        $db->exec();
+    }
 }
-?>
