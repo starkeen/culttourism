2a765f7db7ac4d4cddafb62f88b604919f97ccf4
Andrey Panisko <starkeen@gmail.com>
2015-03-13 09:27:59 +0000

    Вынес код карты страницы списков

---
+++ js/map_page_list.js
@@ -0,0 +1,21 @@
+ymaps.ready(function () {
+        var myMap = new ymaps.Map("list-map", {
+            center: [37, 55],
+            zoom: 10,
+            behaviors: ['default', 'scrollZoom']
+        });
+        myMap.controls.add("zoomControl").add("mapTools").add(new ymaps.control.TypeSelector(["yandex#map", "yandex#hybrid"]));
+        $.getScript('/js/nmap-autoswitcher/nmap-autoswitcher.js', function () {
+            var autoSwitcher = new AutoSwitcher();
+            autoSwitcher.addToMap(myMap);
+        });
+
+        ymaps.geoXml.load("http://culttourism.ru/map/list/?lid=" + $("#list-id").val())
+                .then(function (res) {
+                    var bounds = res.mapState.getBounds();
+                    myMap.geoObjects.add(res.geoObjects);
+                    myMap.setBounds(bounds);
+                },
+                        function (error) {
+                        });
+    });
