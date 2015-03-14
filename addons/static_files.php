eb1a799e1951ad343ca7d5684988911b8acbb4b1
starkeen <starkeen@gmail.com>
2015-03-14 09:10:56 +0400

    Вынес конфиг сборки статики в отдельный файл

---
+++ addons/static_files.php
@@ -0,0 +1,59 @@
+<?php
+
+return array(
+    'css' => array(
+        'common' => array(
+            _DIR_ROOT . '/css/common-layout.css',
+            _DIR_ROOT . '/addons/autocomplete/autocomplete.css',
+            _DIR_ROOT . '/addons/simplemodal/simplemodal.css',
+            _DIR_ROOT . '/css/common-modules.css',
+            _DIR_ROOT . '/css/common-print.css',
+            _DIR_ROOT . '/css/common-media-queries.css',
+        ),
+        'api' => array(
+            _DIR_ROOT . '/css/api.css',
+        ),
+    ),
+    'js' => array(
+        'common' => array(
+            _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
+            _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
+            _DIR_ROOT . '/js/main.js',
+        ),
+        'map' => array(
+            _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
+            _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
+            _DIR_ROOT . '/js/main.js',
+            _DIR_ROOT . '/js/map.js',
+        ),
+        'list' => array(
+            _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
+            _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
+            _DIR_ROOT . '/js/main.js',
+            _DIR_ROOT . '/js/map_page_list.js',
+        ),
+        'city' => array(
+            _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
+            _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
+            _DIR_ROOT . '/js/main.js',
+            _DIR_ROOT . '/js/map_page_city.js',
+            _DIR_ROOT . '/js/adv_city.js',
+        ),
+        'point' => array(
+            _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
+            _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
+            _DIR_ROOT . '/js/main.js',
+            _DIR_ROOT . '/js/adv_point.js',
+            _DIR_ROOT . '/js/map_page_point.js',
+            _DIR_ROOT . '/js/panoramio_page_point.js',
+        ),
+        'api' => array(
+            _DIR_ROOT . '/js/api.js',
+        ),
+        'editor' => array(
+            _DIR_ROOT . '/js/jquery.ui.core.js',
+            _DIR_ROOT . '/js/jquery.ui.datepicker.js',
+            _DIR_ROOT . '/js/jquery.ui.datepicker-ru.js',
+        ),
+    ),
+);
