1bc6559bbd92420f5c8226a592a84550ec023afd
starkeen <starkeen@gmail.com>
2015-03-19 20:49:30 +0400

    Перенес jquery на сервер и обновил версию до последней

--- config/static_files.php
+++ config/static_files.php
@@ -16,23 +16,31 @@ return array(
     ),
     'js' => array(
         'common' => array(
+            _DIR_ROOT . '/addons/jquery/jquery.2.1.3.min.js',
+            _DIR_ROOT . '/addons/jquery/jquery-migrate-1.2.1.min.js',
             _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
             _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
             _DIR_ROOT . '/js/main.js',
         ),
         'map' => array(
+            _DIR_ROOT . '/addons/jquery/jquery.2.1.3.min.js',
+            _DIR_ROOT . '/addons/jquery/jquery-migrate-1.2.1.min.js',
             _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
             _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
             _DIR_ROOT . '/js/main.js',
             _DIR_ROOT . '/js/map.js',
         ),
         'list' => array(
+            _DIR_ROOT . '/addons/jquery/jquery.2.1.3.min.js',
+            _DIR_ROOT . '/addons/jquery/jquery-migrate-1.2.1.min.js',
             _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
             _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
             _DIR_ROOT . '/js/main.js',
             _DIR_ROOT . '/js/map_page_list.js',
         ),
         'city' => array(
+            _DIR_ROOT . '/addons/jquery/jquery.2.1.3.min.js',
+            _DIR_ROOT . '/addons/jquery/jquery-migrate-1.2.1.min.js',
             _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
             _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
             _DIR_ROOT . '/js/main.js',
@@ -40,12 +48,14 @@ return array(
             _DIR_ROOT . '/js/adv_city.js',
         ),
         'point' => array(
+            _DIR_ROOT . '/addons/jquery/jquery.2.1.3.min.js',
+            _DIR_ROOT . '/addons/jquery/jquery-migrate-1.2.1.min.js',
             _DIR_ROOT . '/addons/simplemodal/jquery.simplemodal.1.4.4.min.js',
             _DIR_ROOT . '/addons/autocomplete/jquery.autocomplete.min.js',
             _DIR_ROOT . '/js/main.js',
             _DIR_ROOT . '/js/adv_point.js',
             _DIR_ROOT . '/js/map_page_point.js',
-           // _DIR_ROOT . '/js/panoramio_page_point.js',
+        // _DIR_ROOT . '/js/panoramio_page_point.js',
         ),
         'api' => array(
             _DIR_ROOT . '/js/api.js',
