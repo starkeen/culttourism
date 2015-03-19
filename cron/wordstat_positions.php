51340645025155c9aa941ffbcaa56d3f81b9a5dd
Andrey Panisko <starkeen@gmail.com>
2015-03-19 14:38:59 +0000

    xml.yandex.ru перешел на https

--- cron/wordstat_positions.php
+++ cron/wordstat_positions.php
@@ -44,8 +44,7 @@ DOC;
             "Content-length: " . strlen($doc),
             'content' => $doc
     )));
-    $response = file_get_contents('http://xmlsearch.yandex.ru/xmlsearch?user=starkeen&key=03.10766361:bbf1bd34a06a8c93a745fcca95b31b80&lr=225', true, $context);
-    // $response = file_get_contents('http://xmlsearch.yandex.ru/xmlsearch?user=starkeen&key=03.10766361:bbf1bd34a06a8c93a745fcca95b31b80&lr=1', true, $context);
+    $response = file_get_contents('https://xmlsearch.yandex.ru/xmlsearch?user=starkeen&key=03.10766361:bbf1bd34a06a8c93a745fcca95b31b80&lr=225', true, $context);
     if ($response) {
         $xmldoc = new SimpleXMLElement($response);
         $error = $xmldoc->response->error;
