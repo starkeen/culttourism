d3e8a5c0ba7f891930d78232f71779e3cb85fa9e
starkeen <starkeen@gmail.com>
2015-03-20 21:43:57 +0400

    Страница ошибки 404 на Smarty

--- includes/class.Core.php
+++ includes/class.Core.php
@@ -151,7 +151,7 @@ abstract class Core {
         switch ($err_code) {
             case '301': {
                     header("HTTP/1.1 301 Moved Permanently");
-                    header("Location: ../../../$err_data");
+                    header("Location: $err_data");
                     exit();
                 }
                 break;
@@ -174,10 +174,9 @@ abstract class Core {
 
                     $this->title = "$this->title - 404 Not Found - страница не найдена на сервере";
                     $this->h1 = 'Не найдено';
-                    $this->content = file_get_contents(_DIR_TEMPLATES . '/_errors/er404.sm.html');
-                    $this->content = str_replace('<!--#echo var="REQUEST_URI" -->', $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], $this->content);
-                    $this->content = str_replace('<!--#echo var="HTTP_SIGNATURE" -->', $_SERVER['SERVER_SIGNATURE'], $this->content);
-                    $this->content = str_replace('<!--#echo var="HTTP_HOST" -->', _URL_ROOT, $this->content);
+                    $smarty->assign('requested', $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+                    $smarty->assign('host', _SITE_URL);
+                    $this->content = $smarty->fetch(_DIR_TEMPLATES . '/_errors/er404.sm.html');
                 }
                 break;
             case '503': {
