7d509e0f3c42a39dbb5abccef2862538e5b4feab
starkeen <starkeen@gmail.com>
2015-03-21 23:22:03 +0400

    Заготовка таблицы с активными заявками, see #12

--- _admin/addpoints.php
+++ _admin/addpoints.php
@@ -5,7 +5,18 @@ include (_DIR_INCLUDES . '/class.Pager.php');
 
 $smarty->assign('title', 'Заявки на добавление');
 
-$smarty->assign('content', $smarty->fetch(_DIR_TEMPLATES . '/_admin/adding.list.sm.html'));
+$c = new MCandidatePoints($db);
+
+if (isset($_GET['id'])) {
+    // -----------   обработка заявки ----------
+    $smarty->assign('content', $smarty->fetch(_DIR_TEMPLATES . '/_admin/pointadding.item.sm.html'));
+} else {
+    // -----------   список активных ----------
+    $smarty->assign('list', $c->getActive());
+    $smarty->assign('content', $smarty->fetch(_DIR_TEMPLATES . '/_admin/pointadding.list.sm.html'));
+}
+
+
 
 $smarty->display(_DIR_TEMPLATES . '/_admin/admpage.sm.html');
 exit();
