784646d7cf9c443a2a3e8f6608480bdef0b6e2a6
starkeen <starkeen@gmail.com>
2015-03-26 21:10:31 +0400

    Расширенная страница текущего списка кандидатов, see #12

--- models/class.MCandidatePoints.php
+++ models/class.MCandidatePoints.php
@@ -30,7 +30,9 @@ class MCandidatePoints extends Model {
             'cp_active',
         );
         parent::__construct($db);
+        $this->_addRelatedTable('pagecity');
         $this->_addRelatedTable('uniref_values');
+        $this->_addRelatedTable('ref_pointtypes');
     }
 
     public function add($data) {
@@ -38,6 +40,24 @@ class MCandidatePoints extends Model {
         $data['cp_state'] = 3;
         $data['cp_active'] = 1;
         return $this->insert($data);
+    }
+
+    public function getActive() {
+        $this->_db->sql = "SELECT t.*,
+                                pc.pc_title AS page_title,
+                                uv_stat.uv_title AS state_title,
+                                pt.tp_icon AS type_icon, pt.tp_short AS type_title
+                            FROM $this->_table_name AS t
+                                LEFT JOIN {$this->_tables_related['pagecity']} AS pc
+                                    ON pc.pc_id = t.cp_citypage_id
+                                LEFT JOIN {$this->_tables_related['uniref_values']} AS uv_stat
+                                    ON uv_stat.uv_id = t.cp_state
+                                LEFT JOIN {$this->_tables_related['ref_pointtypes']} AS pt
+                                    ON pt.tp_id = t.cp_type_id
+                            WHERE $this->_table_active = 1
+                            ORDER BY $this->_table_order ASC\n";
+        $this->_db->exec();
+        return $this->_db->fetchAll();
     }
 
 }
