cd794f5fdd347b922e3b5ef609d19b82cc839897
Andrey Panisko <starkeen@gmail.com>
2015-04-29 07:44:03 +0000

    exit after redirect header

--- _admin/common.php
+++ _admin/common.php
@@ -3,6 +3,7 @@
 if(!isset($_SERVER['HTTP_X_HTTPS']) || $_SERVER['HTTP_X_HTTPS'] == "") {
     header("HTTP/1.1 301 Moved Permanently");
     header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    exit();
 }
 
 error_reporting(E_ALL);
