e268565adad3cc66c87d2931b9cda85581481813
Andrey Panisko <starkeen@gmail.com>
2015-04-29 06:47:33 +0000

    redirect to https

--- _admin/common.php
+++ _admin/common.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== "on") {
+if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "") {
     header("HTTP/1.1 301 Moved Permanently");
     header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 }
