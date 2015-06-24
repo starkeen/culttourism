d26bf49fee288948d64f3705ab32849e913e79ac
Andrey Panisko <starkeen@gmail.com>
2015-06-24 10:14:13 +0000

    Почему не работает зум колесом?

--- js/map_page_city.js
+++ js/map_page_city.js
@@ -5,13 +5,8 @@ ymaps.ready(function() {
         type: "yandex#map",
         controls: ["zoomControl", "typeSelector", "geolocationControl"]
     });
-    /*
-    myMap.controls.add("zoomControl").add("mapTools").add(new ymaps.control.TypeSelector(["yandex#map", "yandex#hybrid"]));
-    $.getScript('/js/nmap-autoswitcher/nmap-autoswitcher.js', function() {
-        var autoSwitcher = new AutoSwitcher();
-        autoSwitcher.addToMap(myMap);
-    });
-    */
+    myMap.behaviors.enable("scrollZoom");
+
     ymaps.geoXml.load('https://culttourism.ru/map/city/?cid=' + $('#mapcity_pc_id').val()).then(function(res) {
         myMap.geoObjects.add(res.geoObjects);
         if (res.mapState) {
