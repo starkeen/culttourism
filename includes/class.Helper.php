7d4d41c6b4c1d4948c6fe59eb371fa9409a94c26
starkeen <starkeen@gmail.com>
2015-08-04 21:58:12 +0400

    Автоматическая простановка адресов по точкам из Яндекса, fix #31

--- includes/class.Helper.php
+++ includes/class.Helper.php
@@ -147,6 +147,40 @@ class Helper {
         return(md5(uniqid(mt_rand(), 1)));
     }
 
-}
+    /**
+     * Функция вычисляет расстояние между двумя точками по координатам
+     * 
+     * @param float $lat1
+     * @param float $long1
+     * @param float $lat2
+     * @param float $long2
+     * @return float
+     */
+    
+    public static function distanceGPS($lat1, $long1, $lat2, $long2) {
+        $R = 6372795; // радиус Земли
+        //перевод коордитат в радианы
+        $lat1 *= pi() / 180;
+        $lat2 *= pi() / 180;
+        $long1 *= pi() / 180;
+        $long2 *= pi() / 180;
 
-?>
+        //вычисление косинусов и синусов широт и разницы долгот
+        $cl1 = cos($lat1);
+        $cl2 = cos($lat2);
+        $sl1 = sin($lat1);
+        $sl2 = sin($lat2);
+        $delta = $long2 - $long1;
+        $cdelta = cos($delta);
+        $sdelta = sin($delta);
+
+        //вычисления длины большого круга
+        $y = sqrt(pow($cl2 * $sdelta, 2) + pow($cl1 * $sl2 - $sl1 * $cl2 * $cdelta, 2));
+        $x = $sl1 * $sl2 + $cl1 * $cl2 * $cdelta;
+        $ad = atan2($y, $x);
+        $dist = $ad * $R; //расстояние между двумя координатами в метрах
+
+        return $dist;
+    }
+
+}
