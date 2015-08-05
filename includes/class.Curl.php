7e14e9c2a947c29130be18b407388a417bc55c77
starkeen <starkeen@gmail.com>
2015-08-05 22:58:28 +0400

    Класс для исправления данных в точках, see #39

--- includes/class.Curl.php
+++ includes/class.Curl.php
@@ -12,10 +12,6 @@ class Curl {
         curl_setopt($this->_curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36');
     }
 
-    public function __destruct() {
-        curl_close($this->_curl);
-    }
-
     public function get($url) {
         $text = $this->_cc->get($url);
         if ($text === null) {
@@ -25,6 +21,14 @@ class Curl {
             $this->_cc->put($url, $text);
         }
         return $text;
+    }
+
+    public function config($option, $value) {
+        curl_setopt($this->_curl, $option, $value);
+    }
+
+    public function __destruct() {
+        curl_close($this->_curl);
     }
 
 }
---
+++ includes/class.DataChecker.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Класс для проверки и исправления данных
+ */
+class DataChecker {
+
+    protected $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function repairPointsAddrs($count = 100) {
+        $p = new MPagePoints($this->db);
+
+        $curl = new Curl($this->db);
+        $curl->config(CURLOPT_TIMEOUT, 5);
+        $curl->config(CURLOPT_HEADER, 0);
+        $curl->config(CURLOPT_SSL_VERIFYPEER, false);
+
+        $points = $this->getPointsWithoutAddrs($count);
+        $log = array();
+        foreach ($points as $i => $pt) {
+            $request = 'https://geocode-maps.yandex.ru/1.x/?format=json'
+                    . '&geocode=' . $pt['pt_longitude'] . ',' . $pt['pt_latitude']
+                    . '&ll=37.618920,55.756994'
+                    . '&kind=house&results=1';
+            $answer = $curl->get($request);
+            $data = json_decode($answer);
+            $founded = $data->response->GeoObjectCollection->metaDataProperty->GeocoderResponseMetaData->found > 0;
+            $featureMember = $data->response->GeoObjectCollection->featureMember;
+            foreach ($featureMember as $fm) {
+                $latlon = explode(' ', $fm->GeoObject->Point->pos);
+                $addr_variant = array(
+                    'text' => $fm->GeoObject->metaDataProperty->GeocoderMetaData->text,
+                    'gps' => array(
+                        'latitude' => $latlon[1],
+                        'longitude' => $latlon[0],
+                    ),
+                    'delta_lat' => round(abs($pt['pt_latitude'] - $latlon[1]), 5),
+                    'delta_lon' => round(abs($pt['pt_longitude'] - $latlon[0]), 5),
+                    'delta_meters' => Helper::distanceGPS($pt['pt_latitude'], $pt['pt_longitude'], $latlon[1], $latlon[0]),
+                );
+            }
+            if ($founded && $addr_variant['delta_meters'] < 10) {
+                $p->updateByPk($pt['pt_id'], array(
+                    'pt_adress' => $addr_variant['text'],
+                ));
+                $log[] = array(
+                    $pt['pt_id'],
+                    $pt['pt_name'],
+                    $pt['pt_adress'],
+                    $addr_variant['text'],
+                    round($addr_variant['delta_meters'], 2),
+                );
+            }
+        }
+        return $log;
+    }
+
+    private function getPointsWithoutAddrs($limit = 100) {
+        $dbc = $this->db->getTableName('pagecity');
+        $dbp = $this->db->getTableName('pagepoints');
+        $dbdc = $this->db->getTableName('data_check');
+
+        $this->db->sql = "SELECT pt.pt_id, pt.pt_name, pt.pt_adress,
+                            pt.pt_latitude, pt.pt_longitude,
+                            pc.pc_title, pc.pc_latitude, pc.pc_longitude
+                    FROM $dbp pt
+                        LEFT JOIN $dbc pc ON pc.pc_id = pt.pt_citypage_id
+                        LEFT JOIN $dbdc dc ON dc.dc_item_id = pt.pt_id AND dc.dc_type = 'point' AND dc.dc_field = 'pt_adress'
+                    WHERE pt.pt_active = 1
+                        AND ABS(CHAR_LENGTH(pt.pt_adress)-CHAR_LENGTH(pc.pc_title)) < 6
+                        AND pt.pt_latitude IS NOT NULL
+                    ORDER BY dc.dc_date
+                    LIMIT $limit";
+        $this->db->exec();
+        $points = array();
+        while ($row = $this->db->fetch()) {
+            $points[] = $row;
+        }
+        return $points;
+    }
+
+}
