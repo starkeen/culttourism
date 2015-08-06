1a883ff4970e0511627a0b23a8f63229ef8a5a81
Andrey Panisko <starkeen@gmail.com>
2015-08-06 06:47:08 +0000

    В модельке теперь есть дата истечения кэша

--- models/class.MCurlCache.php
+++ models/class.MCurlCache.php
@@ -12,6 +12,7 @@ class MCurlCache extends Model {
             'cc_date',
             'cc_url',
             'cc_text',
+            'cc_expire',
         );
         parent::__construct($db);
     }
@@ -23,13 +24,17 @@ class MCurlCache extends Model {
         return !empty($row) ? $row['cc_text'] : null;
     }
 
-    public function put($url, $text) {
+    public function put($url, $text, $expire = 30) {
         $this->_db->sql = "INSERT INTO $this->_table_name
                             SET
                                 cc_date = NOW(),
                                 cc_url = '" . $this->escape($url) . "',
-                                cc_text = '" . $this->escape($text) . "'
-                            ON DUPLICATE KEY UPDATE cc_date = NOW(), cc_text = '" . $this->escape($text) . "'";
+                                cc_text = '" . $this->escape($text) . "',
+                                cc_expire = DATE_ADD(NOW(), INTERVAL ".intval($expire)." DAY)
+                            ON DUPLICATE KEY UPDATE
+                                cc_date = NOW(),
+                                cc_text = '" . $this->escape($text) . "',
+                                cc_expire = DATE_ADD(NOW(), INTERVAL ".intval($expire)." DAY)";
         $this->_db->exec();
     }
 
