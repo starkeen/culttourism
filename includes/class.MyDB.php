9ba06635bc8053ad1c1e1d03eb67e054c947d288
Andrey Panisko <starkeen@gmail.com>
2015-08-13 07:11:43 +0000

    Лог по каждому запросу в один файл

--- includes/class.MyDB.php
+++ includes/class.MyDB.php
@@ -26,10 +26,12 @@ class MyDB extends MyPDO {
         $query = $this->sql;
         $query_mask = preg_replace('/\d/', '', $query);
         if (true) {
-            $filename_log = _DIR_DATA . '/logs/sql_log_' . md5($query_mask) . '.log';
-            $filename_sql = _DIR_DATA . '/logs/sql_sql_' . md5($query_mask) . '.log';
-            if (!file_exists($filename_sql)) {
-                file_put_contents($filename_sql, $query . PHP_EOL);
+            $filename_log = _DIR_DATA . '/logs/sql_' . md5($query_mask) . '.log';
+            if (!file_exists($filename_log)) {
+                file_put_contents($filename_log, $query . PHP_EOL);
+                file_put_contents($filename_log, '============' . PHP_EOL, FILE_APPEND);
+                file_put_contents($filename_log, str_pad('', 1000-mb_strlen($query), '#'). PHP_EOL, FILE_APPEND);
+                file_put_contents($filename_log, '------------' . PHP_EOL, FILE_APPEND);
             }
             file_put_contents($filename_log, date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
         }
