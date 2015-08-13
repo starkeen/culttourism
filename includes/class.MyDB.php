57d182eadfc3451c91e6acd8736eef4e28296799
starkeen <starkeen@gmail.com>
2015-08-13 23:18:22 +0400

    Форматирование файла

--- includes/class.MyDB.php
+++ includes/class.MyDB.php
@@ -30,7 +30,7 @@ class MyDB extends MyPDO {
             if (!file_exists($filename_log)) {
                 file_put_contents($filename_log, $query . PHP_EOL);
                 file_put_contents($filename_log, '============' . PHP_EOL, FILE_APPEND);
-                file_put_contents($filename_log, str_pad('', 1000-mb_strlen($query), '#'). PHP_EOL, FILE_APPEND);
+                file_put_contents($filename_log, str_pad('', 1000 - mb_strlen($query), '#') . PHP_EOL, FILE_APPEND);
                 file_put_contents($filename_log, '------------' . PHP_EOL, FILE_APPEND);
             }
             file_put_contents($filename_log, date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
