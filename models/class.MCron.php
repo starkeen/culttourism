924088b48841891eab82d1a15367fa7e117ed824
Andrey Panisko <starkeen@gmail.com>
2015-08-13 12:05:03 +0000

    Основные методы работы с кронами

--- models/class.MCron.php
+++ models/class.MCron.php
@@ -17,11 +17,51 @@ class MCron extends Model {
             'cr_datenext',
             'cr_period',
             'cr_lastresult',
-            'cr_lastexectime,
+            'cr_lastexectime',
             'cr_isrun',
             'cr_active',
         );
         parent::__construct($db);
+    }
+    
+    public function killPhantoms() {
+        $this->_db->sql = "UPDATE $this->_table_name
+            SET cr_isrun = 0
+            WHERE cr_isrun = 1
+            AND cr_active = 1
+            AND cr_datelast_attempt < SUBTIME(NOW(), '02:00:00')";
+        $this->_db->exec();
+    }
+    
+    public function getPortion() {
+        $this->_db->sql = "SELECT *, DATE_FORMAT(cr_period, '%d %H:%i') as period FROM $this->_table_name
+                WHERE cr_active = 1 AND cr_isrun = 0 AND cr_datenext <= NOW()";
+        $this->_db->exec();
+        return $this->_db->fetchAll();
+    }
+    
+    public function markWorkStart($id) {
+        $this->_db->sql = "UPDATE $this->_table_name SET cr_isrun = 1, cr_datelast_attempt = NOW() WHERE cr_id = :crid";
+        $this->_db->prepare();
+        $this->_db->execute(array(
+            ':crid' => $id,
+        ));
+    }
+    
+    public function markWorkFinish($id, $content, $exectime) {
+        $this->_db->sql = "UPDATE $this->_table_name SET
+                    cr_isrun = 0,
+                    cr_lastexectime = :exectime,
+                    cr_lastresult = :content,
+                    cr_datenext = DATE_ADD(cr_datenext, INTERVAL DATE_FORMAT(cr_period, '%d %H:%i') DAY_MINUTE),
+                    cr_datelast = NOW()
+                    WHERE cr_id = :crid";
+        $this->_db->prepare();
+        $this->_db->execute(array(
+            ':crid' => $id,
+            ':content' => $content,
+            ':exectime' => $exectime,
+        ));
     }
 
 }
