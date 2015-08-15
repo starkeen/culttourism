790b6449d44646ac03c5297034f782b913abc282
starkeen <starkeen@gmail.com>
2015-08-16 01:03:26 +0400

    Класс универсального кэширования в файлах, see #44

--- includes/class.Cache.php
+++ includes/class.Cache.php
@@ -5,8 +5,85 @@
  */
 class Cache {
 
-    public function __construct() {
-        
+    protected static $_instance;
+
+    /**
+     * Список доступных кэшей
+     * @var array 
+     */
+    private $config = array(
+        'ref' => array(
+            'dir' => 'refs',
+            'lifetime' => 3600,
+        ),
+    );
+    private $cacheDir = null;
+    private $cacheCurrent = null;
+
+    private function __construct($cache_id) {
+        if (isset($this->config[$cache_id])) {
+            $this->cacheCurrent = $this->config[$cache_id];
+            $this->cacheDir = _DIR_DATA . '/private/cache';
+        }
+    }
+
+    /**
+     * Инстанциатор кэша
+     * @param string $cache
+     * @return object
+     */
+    public static function i($cache) {
+        if (null === self::$_instance) {
+            // создаем новый экземпляр
+            self::$_instance = new self($cache);
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Читаем данные из кэша
+     * @param string $key
+     * @return mixed
+     */
+    public function get($key) {
+        $filename = $this->cacheDir . '/' . $this->cacheCurrent['dir'] . '/' . $key;
+        if (!file_exists($filename)) {
+            return null;
+        }
+        $created = filectime($filename);
+        if (time() - $created > $this->cacheCurrent['lifetime']) {
+            $this->remove($key);
+            return null;
+        }
+        return unserialize(file_get_contents($filename));
+    }
+
+    /**
+     * Записываем данные в кэш
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    public function put($key, $value) {
+        $filedir = $this->cacheDir . '/' . $this->cacheCurrent['dir'] . '/';
+        if (!file_exists($filedir)) {
+            mkdir($filedir);
+        }
+        return file_put_contents($filedir . $key, serialize($value), LOCK_EX) > 0;
+    }
+
+    /**
+     * Удаляем ключ из кэша
+     * @param string $key
+     * @return bool
+     */
+    public function remove($key) {
+        $filename = $this->cacheDir . '/' . $this->cacheCurrent['dir'] . '/' . $key;
+        if (file_exists($filename)) {
+            return unlink($filename);
+        } else {
+            return null;
+        }
     }
 
 }
