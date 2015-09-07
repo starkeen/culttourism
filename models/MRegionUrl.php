9188a43f2be5f18320e91ecf3234bf42739fb083
Andrey Panisko <starkeen@gmail.com>
2015-09-07 10:18:38 +0000

    Модель для таблички region_url

---
+++ models/MRegionUrl.php
@@ -0,0 +1,18 @@
+<?php
+
+class MLogActions extends Model {
+
+    protected $_table_pk = 'uid';
+    protected $_table_order = 'uid';
+    protected $_table_active = 'uid';
+
+    public function __construct($db) {
+        $this->_table_name = $db->getTableName('region_url');
+        $this->_table_fields = array(
+            'url',
+            'citypage',
+        );
+        parent::__construct($db);
+    }
+
+}
