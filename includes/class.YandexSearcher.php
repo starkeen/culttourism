ff642ebd55530ce373f3bc336b44c2235fe2039f
Andrey <Andrey@strkns>
2015-10-05 21:59:45 +0300

    Рефакторинг класса поисковика Яндекса, see #49

--- includes/class.YandexSearcher.php
+++ includes/class.YandexSearcher.php
@@ -2,95 +2,67 @@
 
 class YandexSearcher {
 
-    private $_request_url = "https://yandex.ru/search/xml?user=starkeen&key=03.10766361:bbf1bd34a06a8c93a745fcca95b31b80&l10n=ru&sortby=rlv&filter=strict";
-    private $_meta = array(
+    protected $requestURL = "https://yandex.ru/search/xml?user=starkeen&key=03.10766361:bbf1bd34a06a8c93a745fcca95b31b80&l10n=ru&sortby=rlv&filter=strict";
+    protected $meta = array(
         'page' => 0,
         'pages' => 20,
+        'pages_cnt' => 0,
+        'error' => null,
     );
-    private $_enable_logging = false;
-    private $_logger = null;
 
+    /**
+     *
+     * @var boolean
+     */
+    protected $loggingEnabled = false;
+
+    /**
+     *
+     * @var MSearchLog
+     */
+    protected $logger = null;
+
+    /**
+     * Выполнение поискового запроса
+     * @param string $request
+     * @return array
+     * @throws Exception
+     */
     public function search($request) {
         $out = array(
-            'found' => null,
             'results' => null,
             'pages_cnt' => null,
-            'error' => null,
             'error_code' => null,
             'error_text' => null,
         );
-        $query = html_entity_decode($request, ENT_QUOTES, "utf-8");
-        $doc = <<<DOC
-<?xml version="1.0" encoding="utf-8"?>
-<request>
-        <query>$query</query>
-        <page>{$this->_meta['page']}</page>
-        <sortby order="descending" priority="no">rlv</sortby>
-        <maxpassages>5</maxpassages>
-        <groupings>
-            <groupby attr="" mode="flat" groups-on-page="{$this->_meta['pages']}" docs-in-group="1" curcateg="-1" />
-        </groupings>
-</request>
-DOC;
-
+        $doc = $this->buildQuery($request);
         try {
-            if ($this->_enable_logging) {
-                $this->_logger->add(array(
+            if ($this->loggingEnabled) {
+                $this->logger->add(array(
                     'sl_query' => $request,
                     'sl_request' => $doc,
                     'sl_error_code' => 0,
                 ));
             }
-            $context = stream_context_create(array(
-                'http' => array(
-                    'method' => "POST",
-                    'header' => "Content-type: application/xml\r\nContent-length: " . strlen($doc),
-                    'content' => $doc,
-                ),
-                'socket' => array(
-                    'bindto' => '188.225.12.25:0',
-                ),
-            ));
-            $response = @file_get_contents($this->_request_url, true, $context);
+            $response = $this->getRequest($doc);
             if ($response) {
-                if ($this->_enable_logging) {
-                    $this->_logger->setAnswer(array(
+                if ($this->loggingEnabled) {
+                    $this->logger->setAnswer(array(
                         'sl_answer' => $response,
                     ));
                 }
-                $xmldoc = new SimpleXMLElement($response);
+                $results = $this->parseResponse($response);
 
-                $out['error'] = $xmldoc->response->error;
-                $out['found'] = $xmldoc->xpath("response/results/grouping/group/doc");
-                if (empty($out['error'])) {
-                    $out['pages_cnt'] = (int) $xmldoc->response->found;
-                    foreach ($out['found'] as $item) {
-                        $result_item = array(
-                            'domain' => (string) $item->domain,
-                            'url' => (string) $item->url,
-                            'title' => $this->clean($item->title),
-                            'title_hw' => $this->highlight($item->title),
-                            'descr' => '',
-                            'descr_hw' => '',
-                        );
-                        if ($item->passages) {
-                            foreach ($item->passages->passage as $passage) {
-                                $result_item['descr'] .= $this->clean($passage) . "\n";
-                                $result_item['descr_hw'] .= $this->highlight($passage);
-                            }
-                        }
-                        $result_item['descr'] = trim($result_item['descr']);
-                        $result_item['descr_hw'] = trim($result_item['descr_hw']);
-                        $out['results'][] = $result_item;
-                    }
-                } else {
-                    $err_attr = $out['error']->attributes();
-                    $out['error_code'] = $err_attr['code'];
-                    $out['error_text'] = "Ошибка: " . $out['error'];
-                    if ($this->_enable_logging) {
-                        $this->_logger->setAnswer(array(
-                            'sl_error_code' => $out['error_code'],
-                            'sl_error_text' => (string) $out['error'],
+                if (!empty($results)) {
+                    $out['results'] = $results;
+                    $out['pages_cnt'] = $this->meta['pages_cnt'];
+                } elseif (!empty($this->meta['error'])) {
+                    $out['error_code'] = $this->meta['error_code'];
+                    $out['error_text'] = $this->meta['error_text'];
+                    if ($this->loggingEnabled) {
+                        $this->logger->setAnswer(array(
+                            'sl_error_code' => $this->meta['error_code'],
+                            'sl_error_text' => (string) $this->meta['error'],
                         ));
                     }
                 }
@@ -101,32 +73,134 @@ DOC;
         } catch (Exception $e) {
             $out['error_text'] .= ' ' . $e->getMessage();
         }
-        unset($out['found']);
-        unset($out['error']);
         return $out;
     }
 
+    /**
+     * Установка текущей страницы выдачи
+     * @param type $page
+     */
     public function setPage($page) {
-        $this->_meta['page'] = intval($page);
+        $this->meta['page'] = intval($page);
     }
 
+    /**
+     * Установка максимума страниц в выдаче
+     * @param type $max
+     */
     public function setPagesMax($max) {
-        $this->_meta['pages'] = intval($max);
+        $this->meta['pages'] = intval($max);
     }
 
+    /**
+     * Включение лога запросов
+     * @param type $db
+     */
     public function enableLogging($db) {
-        $this->_logger = new MSearchLog($db);
-        $this->_enable_logging = true;
+        $this->logger = new MSearchLog($db);
+        $this->loggingEnabled = true;
     }
 
+    /**
+     * Добавление тегов strong в подсветку
+     * @param type $node
+     * @return type
+     */
     private function highlight($node) {
         $stripped = preg_replace('/<\/?(title|passage)[^>]*>/', '', $node->asXML());
         return str_replace('</hlword>', '</strong>', preg_replace('/<hlword[^>]*>/', '<strong>', $stripped));
     }
 
+    /**
+     * Очистка строки XML от некоторых тегов
+     * @param type $node
+     * @return type
+     */
     private function clean($node) {
         $stripped = preg_replace('/<\/?(title|passage)[^>]*>/', '', $node->asXML());
         return str_replace('</hlword>', '', preg_replace('/<hlword[^>]*>/', '', $stripped));
+    }
+
+    /**
+     * Построение XML-запроса
+     * @param string $request - поисковая строка
+     * @return string
+     */
+    protected function buildQuery($request) {
+        $query = html_entity_decode($request, ENT_QUOTES, "utf-8");
+        $doc = <<<DOC
+<?xml version="1.0" encoding="utf-8"?>
+<request>
+        <query>$query</query>
+        <page>{$this->meta['page']}</page>
+        <sortby order="descending" priority="no">rlv</sortby>
+        <maxpassages>5</maxpassages>
+        <groupings>
+            <groupby attr="" mode="flat" groups-on-page="{$this->meta['pages']}" docs-in-group="1" curcateg="-1" />
+        </groupings>
+</request>
+DOC;
+        return $doc;
+    }
+
+    /**
+     * Разбор ответа сервера
+     * @param string $response
+     * @return array
+     */
+    protected function parseResponse($response) {
+        $results = array();
+        $xmldoc = new SimpleXMLElement($response);
+
+        $this->meta['error'] = $xmldoc->response->error;
+        $found = $xmldoc->xpath("response/results/grouping/group/doc");
+        if (empty($this->meta['error'])) {
+            $this->meta['pages_cnt'] = (int) $xmldoc->response->found;
+            foreach ($found as $item) {
+                $result_item = array(
+                    'domain' => (string) $item->domain,
+                    'url' => (string) $item->url,
+                    'title' => $this->clean($item->title),
+                    'title_hw' => $this->highlight($item->title),
+                    'descr' => '',
+                    'descr_hw' => '',
+                );
+                if ($item->passages) {
+                    foreach ($item->passages->passage as $passage) {
+                        $result_item['descr'] .= $this->clean($passage) . "\n";
+                        $result_item['descr_hw'] .= $this->highlight($passage);
+                    }
+                }
+                $result_item['descr'] = trim($result_item['descr']);
+                $result_item['descr_hw'] = trim($result_item['descr_hw']);
+                $results[] = $result_item;
+            }
+        } else {
+            $err_attr = $this->meta['error']->attributes();
+            $this->meta['error_code'] = $err_attr['code'];
+            $this->meta['error_text'] = "Ошибка: " . $this->meta['error'];
+        }
+        return $results;
+    }
+
+    /**
+     * Метод для запросов к Яндексу
+     * @param string $data - XML запрос
+     * @return mixed ответ сервера Яндекса
+     */
+    protected function getRequest($data) {
+        $context = stream_context_create(array(
+            'http' => array(
+                'method' => "POST",
+                'header' => "Content-type: application/xml\r\nContent-length: " . strlen($data),
+                'content' => $data,
+            ),
+            'socket' => array(
+                'bindto' => '188.225.12.25:0',
+            ),
+        ));
+        $response = @file_get_contents($this->requestURL, true, $context);
+        return $response;
     }
 
 }
