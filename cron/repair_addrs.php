ea159a7672bc0bec8efff746d900750f97c68675
Andrey Panisko <starkeen@gmail.com>
2015-11-23 19:59:09 +0300

    Все работы по исправлению данных в одном скрипте

--- cron/repair_addrs.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$checker = new DataChecker($db);
-$log = $checker->repairPointsAddrs(30);
-
-if (!empty($log)) {
-    print_r($log);
-}
--- cron/repair_data.php
+++ cron/repair_data.php
@@ -1,4 +1,20 @@
 <?php
 
+$checker = new DataChecker($db);
+$pc = new MPageCities($db);
+$p = new MPagePoints($db);
 $pt = new MPagePoints($db);
+$ls = new MLists($db);
+$bg = new MBlogEntries($db);
+
+$log = $checker->repairPointsAddrs(30);
+if (!empty($log)) {
+    print_r($log);
+}
+
 $pt->repairPhones();
+
+$pt->repairLinksAbsRel();
+$pc->repairLinksAbsRel();
+$ls->repairLinksAbsRel();
+$bg->repairLinksAbsRel();
