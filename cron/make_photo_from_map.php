c842aa17c66441e831672d2b8e41828d3048bd89
Andrey Panisko <starkeen@gmail.com>
2016-04-10 22:37:50 +0300

    Проверка получения урла карты

--- cron/make_photo_from_map.php
+++ cron/make_photo_from_map.php
@@ -1,4 +1,13 @@
 <?php
 
+$dirSave = _DIR_DATA . '/photos';
+$dirRelative = '/data/photos';
+$googleAPIkey = 'AIzaSyBiuHllm_OCLEKww8y02DJPeePMtvEnTiE';
+$size = 500;
 $ph = new MPhotos($db);
+
 $cities = $ph->getCityPagesWithoutPhotos();
+foreach ($cities as $pc) {
+    $url = sprintf("https://maps.googleapis.com/maps/api/staticmap?center=%s,%s6&zoom=%s&size=%sx%s&maptype=roadmap&key=%s", $pc['pc_latitude'], $pc['pc_longitude'], $pc['pc_latlon_zoom'], $size, $size, $googleAPIkey);
+    echo $url, "\n";
+}
