123e67006ac38a15be29103f7ba2929d37099257
Andrey Panisko <starkeen@gmail.com>
2016-04-11 22:12:55 +0300

    Полноценный правильный загрузчик

--- cron/make_photo_from_map.php
+++ cron/make_photo_from_map.php
@@ -5,6 +5,7 @@ $dirRelative = '/data/photos';
 $googleAPIkey = 'AIzaSyBiuHllm_OCLEKww8y02DJPeePMtvEnTiE';
 $size = 500;
 $ph = new MPhotos($db);
+$city = new MPageCities($db);
 
 $cities = $ph->getCityPagesWithoutPhotos();
 foreach ($cities as $pc) {
@@ -14,5 +15,22 @@ foreach ($cities as $pc) {
             . translit(str_replace(' ', '_', preg_replace("/[^a-zA-ZА-Яа-я0-9ё\s]/ui", '', mb_strtolower($pc['pc_title_unique']))))
             . '.png';
 
-    copy($url, $dirSave . '/' . $fileName);
+    if (copy($url, $dirSave . '/' . $fileName)) {
+        $id = $ph->insert(array(
+            'ph_src' => $dirRelative . '/' . $fileName,
+            'ph_title' => $pc['pc_title_unique'],
+            'ph_author' => 'Google Maps',
+            'ph_width' => $size,
+            'ph_height' => $size,
+            'ph_lat' => $pc['pc_latitude'],
+            'ph_lon' => $pc['pc_longitude'],
+            'ph_date_add' => $ph->now(),
+        ));
+
+        $city->updateByPk($pc['pc_id'], array(
+            'pc_coverphoto_id' => $id,
+        ));
+    } else {
+        echo 'Ошибка загрузки карты: ', $pc['pc_title_unique'];
+    }
 }
