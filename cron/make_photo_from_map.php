85cd7733705c703db43cb83e008025b8c30c4d26
Andrey Panisko <starkeen@gmail.com>
2016-04-11 21:59:31 +0300

    Сохранение файлов карт городов на диске

--- cron/make_photo_from_map.php
+++ cron/make_photo_from_map.php
@@ -8,6 +8,11 @@ $ph = new MPhotos($db);
 
 $cities = $ph->getCityPagesWithoutPhotos();
 foreach ($cities as $pc) {
-    $url = sprintf("https://maps.googleapis.com/maps/api/staticmap?center=%s,%s6&zoom=%s&size=%sx%s&maptype=roadmap&key=%s", $pc['pc_latitude'], $pc['pc_longitude'], $pc['pc_latlon_zoom'], $size, $size, $googleAPIkey);
+    $url = sprintf("https://maps.googleapis.com/maps/api/staticmap?center=%F,%F&zoom=%d&size=%dx%d&maptype=roadmap&key=%s", $pc['pc_latitude'], $pc['pc_longitude'], $pc['pc_latlon_zoom'], $size, $size, $googleAPIkey);
     echo $url, "\n";
+
+    $fileName = sprintf('map_%dx%d_', $size)
+            . str_replace(' ', '_', preg_replace("/[^a-zA-ZА-Яа-я0-9ё\s]/ui", '', mb_strtolower($pc['pc_title_unique'])));
+
+    copy($url, $dirSave . '/' . $fileName);
 }
