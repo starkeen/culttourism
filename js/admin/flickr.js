e85046b244267c2460f930a71a441b9b4cc3832b
Andrey Panisko <starkeen@gmail.com>
2016-06-06 11:38:19 +0000

    В подсказках отображаем нижнюю границу пользовательского интереса

--- js/admin/flickr.js
+++ js/admin/flickr.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
         $.get("flickr.php?act=suggestions", function (response) {
             $("#flickr-suggestions ul").empty();
             $.each(response.data, function (index, value) {
-                $("#flickr-suggestions ul").append("<li>" + value.pc_title + "</li>");
+                $("#flickr-suggestions ul").append("<li>" + value.pc_title + " ("+ value.ws_weight_min +")" + "</li>");
             });
             console.log(response.data);
         });
