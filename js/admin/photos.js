7486ce0114415aa9f91a1345e6b404fe9c69f79c
Andrey Panisko <starkeen@gmail.com>
2016-06-10 22:33:14 +0300

    Саджест региона для привязки, see #60

--- js/admin/photos.js
+++ js/admin/photos.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    //
+    $("#photos-upload-bind-pc").autocomplete({
+        serviceUrl: "/search/suggest/",
+        minChars: 2,
+        paramName: "query",
+        width: 400,
+        onSelect: function (suggestion) {
+            $("#photos-upload-bind-pcid").val(suggestion.data);
+        }
+    });
+    $("#photos-upload-bind-pc-clean").on("click", function () {
+        $("#photos-upload-bind-pc").text("");
+        $("#photos-upload-bind-pcid").val(0);
+    });
 });
