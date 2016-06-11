45b4072a975c951dfa0d634418a489ed6fa51368
Andrey Panisko <starkeen@gmail.com>
2016-06-11 10:06:47 +0300

    отказ от функции

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-function myprog() {
+
   echo "deploy start"
 
   cd /home/s/starkeen/culttourism.ru/public_html
@@ -8,6 +8,3 @@ function myprog() {
   /usr/local/bin/php bin/composer.phar install
 
   echo "deploy finish"
-}
-
-myprog()
