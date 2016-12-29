cea536edeb1d21b1d47c05e2e2dfd6015971a80e
Andrey Panisko <starkeen@gmail.com>
2016-12-29 22:48:31 +0300

    Поправил неймспейсы, see #62

--- src/includes/RSSGenerator.php
+++ src/includes/RSSGenerator.php
@@ -50,9 +50,9 @@ class RSSGenerator
             $item->addChild('link', $entity['link']);
             $item->addChild('pubDate', $entity['date']);
             $item->addChildWithCData('description',$entity['text']);
-            $item->addChildWithCData('content:encoded', $entity['text'], 'content');
+            $item->addChildWithCData('content:encoded', $entity['text'], 'http://purl.org/rss/1.0/modules/content/');
             $item->addChild('author', $entity['author']);
-            $item->addChild('dc:creator', $entity['author'], 'dc');
+            $item->addChild('dc:creator', $entity['author'], 'http://purl.org/dc/elements/1.1/');
         }
 
         return $this->xml->asXML();
