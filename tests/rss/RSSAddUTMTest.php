3c051c537a461a931e71d51cce7570664686271f
Андрей Панисько <starkeen@gmail.com>
2017-04-25 22:20:22 +0300

    Добавил тестовые последовательности для теста UTM

--- tests/rss/RSSAddUTMTest.php
+++ tests/rss/RSSAddUTMTest.php
@@ -16,8 +16,9 @@ class RSSAddUTMTest extends TestCase
                                 $out = [];
                                 foreach ($arg as $item) {
                                     $out[] = [
-                                        'title' => $item['title'],
-                                        'text' => $item['text'],
+                                        'br_title' => $item['br_title'],
+                                        'br_text' => $item['br_text'],
+                                        'br_text_absolute' => $item['br_text_absolute'],
                                     ];
                                 }
                                 return $out;
@@ -46,22 +47,158 @@ class RSSAddUTMTest extends TestCase
             [
                 [
                     [
-                        'title' => 'title 1',
-                        'text' => 'text 1',
+                        'br_title' => 'link without slash',
+                        'br_text' => '<p>before <a href="https://host.tld">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld" target="_blank">link</a> after</p>',
                     ],
                     [
-                        'title' => 'title 2',
-                        'text' => 'text 2',
+                        'br_title' => 'link with slash',
+                        'br_text' => '<p>before <a href="https://host.tld/path/">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/">link</a> after</p>',
+                    ],
+                    [
+                        'br_title' => 'link in second paragraph',
+                        'br_text' => '
+                            <p>first</p>
+                            <p>
+                                before
+                                <a href="https://host.tld/path/">link</a>
+                                 after
+                            </p>
+                            
+                            <p>last</p>
+                        ',
+                        'br_text_absolute' => '
+                            <p>first</p>
+                            <p>
+                                before
+                                <a href="https://host.tld/path/">link</a>
+                                 after
+                            </p>
+                            
+                            <p>last</p>
+                        ',
+                    ],
+                    [
+                        'br_title' => 'link with page in second paragraph',
+                        'br_text' => '
+                            <p>first</p>
+                            <p>
+                                before
+                                <a href="https://host.tld/path/page.html">link</a>
+                                 after
+                            </p>
+                            
+                            <p>last</p>
+                        ',
+                        'br_text_absolute' => '
+                            <p>first</p>
+                            <p>
+                                before
+                                <a href="https://host.tld/path/page.html">link</a>
+                                 after
+                            </p>
+                            
+                            <p>last</p>
+                        ',
+                    ],
+                    [
+                        'br_title' => 'link with slash and query',
+                        'br_text' => '<p>before <a href="https://host.tld/path/?a=1&b[]=2&c">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/?a=1&b[]=2&c">link</a> after</p>',
+                    ],
+                    [
+                        'br_title' => 'link with page and query',
+                        'br_text' => '<p>before <a href="https://host.tld/path/page.html?a=1&b[]=2&c">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/page.html?a=1&b[]=2&c">link</a> after</p>',
+                    ],
+                    [
+                        'br_title' => 'link with page and encoded query',
+                        'br_text' => '<p>before <a href="https://host.tld/path/page.html?a=1&geo%5Bt%5D%5B14%5D=14&c">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/page.html?a=1&geo%5Bt%5D%5B14%5D=14&c">link</a> after</p>',
+                    ],
+                    [
+                        'br_title' => 'link with page and encoded cyrillic query',
+                        'br_text' => '<p>before <a href="https://host.tld/path/page.html?a=1&key=%D1%81%D0%BB%D0%BE%D0%B2%D0%BE&c">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/page.html?a=1&key=%D1%81%D0%BB%D0%BE%D0%B2%D0%BE&c">link</a> after</p>',
                     ],
                 ],
                 [
                     [
-                        'title' => 'title 1',
-                        'text' => 'text 1',
+                        'br_title' => 'link without slash',
+                        'br_text' => '<p>before <a href="https://host.tld">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld" target="_blank">link</a> after</p>',
                     ],
                     [
-                        'title' => 'title 2',
-                        'text' => 'text 2',
+                        'br_title' => 'link with slash',
+                        'br_text' => '<p>before <a href="https://host.tld/path/">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/">link</a> after</p>',
+                    ],
+                    [
+                        'br_title' => 'link in second paragraph',
+                        'br_text' => '
+                            <p>first</p>
+                            <p>
+                                before
+                                <a href="https://host.tld/path/">link</a>
+                                 after
+                            </p>
+                            
+                            <p>last</p>
+                        ',
+                        'br_text_absolute' => '
+                            <p>first</p>
+                            <p>
+                                before
+                                <a href="https://host.tld/path/">link</a>
+                                 after
+                            </p>
+                            
+                            <p>last</p>
+                        ',
+                    ],
+                    [
+                        'br_title' => 'link with page in second paragraph',
+                        'br_text' => '
+                            <p>first</p>
+                            <p>
+                                before
+                                <a href="https://host.tld/path/page.html">link</a>
+                                 after
+                            </p>
+                            
+                            <p>last</p>
+                        ',
+                        'br_text_absolute' => '
+                            <p>first</p>
+                            <p>
+                                before
+                                <a href="https://host.tld/path/page.html">link</a>
+                                 after
+                            </p>
+                            
+                            <p>last</p>
+                        ',
+                    ],
+                    [
+                        'br_title' => 'link with slash and query',
+                        'br_text' => '<p>before <a href="https://host.tld/path/?a=1&b[]=2&c">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/?a=1&b[]=2&c">link</a> after</p>',
+                    ],
+                    [
+                        'br_title' => 'link with page and query',
+                        'br_text' => '<p>before <a href="https://host.tld/path/page.html?a=1&b[]=2&c">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/page.html?a=1&b[]=2&c">link</a> after</p>',
+                    ],
+                    [
+                        'br_title' => 'link with page and encoded query',
+                        'br_text' => '<p>before <a href="https://host.tld/path/page.html?a=1&geo%5Bt%5D%5B14%5D=14&c">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/page.html?a=1&geo%5Bt%5D%5B14%5D=14&c">link</a> after</p>',
+                    ],
+                    [
+                        'br_title' => 'link with page and encoded cyrillic query',
+                        'br_text' => '<p>before <a href="https://host.tld/path/page.html?a=1&key=%D1%81%D0%BB%D0%BE%D0%B2%D0%BE&c">link</a> after</p>',
+                        'br_text_absolute' => '<p>before <a href="https://host.tld/path/page.html?a=1&key=%D1%81%D0%BB%D0%BE%D0%B2%D0%BE&c">link</a> after</p>',
                     ],
                 ],
             ],
