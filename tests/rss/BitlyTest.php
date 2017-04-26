7acab14e357565513ade9a76e55837440caf79c0
Андрей Панисько <starkeen@gmail.com>
2017-04-26 23:06:20 +0300

    Тесты для случаев, когда сервис отвечает ошибкой

--- tests/rss/BitlyTest.php
+++ tests/rss/BitlyTest.php
@@ -25,8 +25,12 @@ class BitlyTest extends TestCase
                                          ->getMock();
     }
 
+    /**
+     * Нормальный полный цикл
+     */
     public function testShorterNormal()
     {
+        $input = 'http://host.tld/';
         $expected = 'https://short.url';
         $answer = json_encode(
             [
@@ -57,7 +61,79 @@ class BitlyTest extends TestCase
         $bitly->setHost('https://bit.ly');
         $bitly->setToken('[token]');
 
-        $result = $bitly->short('http://host.tld/');
+        $result = $bitly->short($input);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * В теле ответа код с ошибкой
+     */
+    public function testShorterErrorStatusCode()
+    {
+        $input = 'http://host.tld/';
+        $expected = $input;
+        $answer = json_encode(
+            [
+                'status_code' => 100500,
+                'data' => [
+                    'url' => 'undefined',
+                ],
+            ]
+        );
+
+        $this->guzzleResponseBody->expects($this->once())
+                                 ->method('getContents')
+                                 ->willReturn($answer);
+        $this->guzzleResponse->expects($this->once())
+                             ->method('getStatusCode')
+                             ->willReturn(200);
+        $this->guzzleResponse->expects($this->once())
+                             ->method('getBody')
+                             ->willReturn($this->guzzleResponseBody);
+        $this->guzzleClient->expects($this->once())
+                           ->method('get')
+                           ->with(
+                               'https://bit.ly/v3/shorten?access_token=[token]&longUrl=http%3A%2F%2Fhost.tld%2F&format=json'
+                           )
+                           ->willReturn($this->guzzleResponse);
+
+        $bitly = new Bitly($this->guzzleClient);
+        $bitly->setHost('https://bit.ly');
+        $bitly->setToken('[token]');
+
+        $result = $bitly->short($input);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * В заголовке ответа код с ошибкой
+     */
+    public function testShorterErrorHeaderCode()
+    {
+        $input = 'http://host.tld/';
+        $expected = $input;
+
+        $this->guzzleResponseBody->expects($this->never())
+                                 ->method('getContents');
+        $this->guzzleResponse->expects($this->once())
+                             ->method('getStatusCode')
+                             ->willReturn(100500);
+        $this->guzzleResponse->expects($this->never())
+                             ->method('getBody');
+        $this->guzzleClient->expects($this->once())
+                           ->method('get')
+                           ->with(
+                               'https://bit.ly/v3/shorten?access_token=[token]&longUrl=http%3A%2F%2Fhost.tld%2F&format=json'
+                           )
+                           ->willReturn($this->guzzleResponse);
+
+        $bitly = new Bitly($this->guzzleClient);
+        $bitly->setHost('https://bit.ly');
+        $bitly->setToken('[token]');
+
+        $result = $bitly->short($input);
 
         $this->assertEquals($expected, $result);
     }
