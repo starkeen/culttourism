3be89abaf3a9c968d2f9a06d5d788436bb8cc540
Андрей Панисько <starkeen@gmail.com>
2017-04-28 22:10:06 +0300

    Абстрактный класс для компонентов RSS

--- src/includes/RSSAddUTM.php
+++ src/includes/RSSAddUTM.php
@@ -1,38 +1,17 @@
 <?php
 
+use app\rss\RSSComponent;
+
 /**
  * Компонент для добавления меток utm_ к ссылкам в RSS
  */
-class RSSAddUTM implements IRSSGenerator
+class RSSAddUTM extends RSSComponent
 {
-    /** @var RSSGenerator */
-    private $generator;
-
-    public function __construct(IRSSGenerator $generator)
-    {
-        $this->generator = $generator;
-    }
-
-    public function __get($name)
-    {
-        return $this->generator->{$name} ?? null;
-    }
-
-    public function __isset($name)
-    {
-        return $this->generator->{$name} !== null;
-    }
-
-    public function __set($name, $value)
-    {
-        $this->generator->{$name} = $value;
-    }
-
     /**
      * @param array $data
      * @return string
      */
-    public function process(array $data)
+    public function process(array $data): string
     {
         return $this->generator->process($data);
     }
---
+++ src/rss/RSSComponent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\rss;
+
+use IRSSGenerator;
+use RSSGenerator;
+
+abstract class RSSComponent implements IRSSGenerator
+{
+    /** @var RSSGenerator */
+    protected $generator;
+
+    public function __construct(IRSSGenerator $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    public function __get($name)
+    {
+        return $this->generator->{$name} ?? null;
+    }
+
+    public function __isset($name)
+    {
+        return $this->generator->{$name} !== null;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->generator->{$name} = $value;
+    }
+
+    abstract public function process(array $data): string;
+}
