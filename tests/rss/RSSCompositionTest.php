2176a5cefa39d50b0a09909df501f67cc08000a8
Андрей Панисько <starkeen@gmail.com>
2017-06-01 12:18:39 +0300

    Тест правильности композиции компонентов RSS

---
+++ tests/rss/RSSCompositionTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\rss;
+
+use PHPUnit\Framework\TestCase;
+
+class RSSCompositionTest extends TestCase
+{
+    public function testComposition()
+    {}
+}
