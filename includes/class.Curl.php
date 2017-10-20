16beb12ee01d93579cffcd4c79f2a296a87f775a
Андрей Панисько <starkeen@gmail.com>
2017-10-20 22:22:12 +0300

    Добавим теперь и транслит

--- includes/class.Curl.php
+++ includes/class.Curl.php
@@ -45,7 +45,7 @@ class Curl
                 $text = curl_exec($this->curl);
                 if ($this->encoding !== self::INTERNAL_ENCODING) {
                     $text = mb_convert_encoding($text, self::INTERNAL_ENCODING, $this->encoding);
-                    $text = iconv($this->encoding, self::INTERNAL_ENCODING . '//IGNORE', $text);
+                    $text = iconv($this->encoding, self::INTERNAL_ENCODING . '//IGNORE//TRANSLIT', $text);
                 }
             } catch (Exception $e) {
                 //echo $e->getMessage();
