21cac4a560803193a5d0d0c79b854f06dca519f2
Андрей Панисько <starkeen@gmail.com>
2017-10-22 18:08:21 +0300

    В логе пишем реферер раньше агента

--- includes/class.Page.php
+++ includes/class.Page.php
@@ -414,7 +414,7 @@ class Page extends PageCommon
         }
 
         if (true || !$skip) {
-            $logEntry = date('Y-m-d H:i:s') . "\t" . $ua . "\t" . $referer . "\n";
+            $logEntry = date('Y-m-d H:i:s') . "\t" . $referer . "\t" . $ua . "\n";
             file_put_contents($logFile, $logEntry, FILE_APPEND | LOCK_EX);
         }
 
