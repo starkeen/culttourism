b80e442d55d07d536c2c060449ea55324d4176f1
Андрей Панисько <starkeen@gmail.com>
2017-10-22 18:04:33 +0300

    Пишем в лог запросы по старым адресам точек -  - пока для всех агентов

--- includes/class.Page.php
+++ includes/class.Page.php
@@ -1,16 +1,34 @@
 <?php
 
-class Page extends PageCommon {
-
+class Page extends PageCommon
+{
     const DESCRIPTION_TRESHOLD = 200;
-    
+
     const REDIR_SUFFIXES = [
         'undefined',
         'com.google.android.googlequicksearchbox',
         'android-app%3A',
     ];
+    const BOT_MARKERS =  [
+        'YandexMetrika',
+        'Googlebot',
+        'YandexBot',
+        'bingbot',
+        'MegaIndex',
+        'MJ12bot',
+        'openstat',
+        'statdom',
+        'Yahoo! Slurp',
+        'SurveyBot',
+        'curl',
+        'wget',
+        'package http',
+        'Xenu Link Sleuth',
+        'archive.org_bot',
+    ];
 
-    public function __construct($db, $mod) {
+    public function __construct($db, $mod)
+    {
         parent::__construct($db, $mod[0]); //встроеные модули
         //if ($mod[1]!=null) $this->getSubContent($this->md_id, $mod[1]);
         //$this->navibar = $this->getNavigation($this->md_id, $mod[1]);
@@ -22,22 +40,28 @@ class Page extends PageCommon {
         }
     }
 
-    public function getPageByURL($aurl) {
+    /**
+     * @param array $aurl
+     *
+     * @return bool|string|void
+     */
+    public function getPageByURL($aurl)
+    {
         $url = '';
         foreach ($aurl as $w) {
-            if ($w != '') {
+            if ($w !== '') {
                 $url .= '/' . $w;
             }
         }
-        if ($url != '') {
+        if ($url !== '') {
             $this->checkRedirect($url);
 
-            $regs = array();
-            $url_parts_array = !empty($url) ? explode('/', $url) : array();
+            $regs = [];
+            $url_parts_array = !empty($url) ? explode('/', $url) : [];
             $urlParts = array_pop($url_parts_array);
-            if ($urlParts == 'map.html') {
+            if ($urlParts === 'map.html') {
                 return $this->getPageMap($url);
-            } elseif ($urlParts == 'index.html') {
+            } elseif ($urlParts === 'index.html') {
                 return $this->getPageCity($url);
             } elseif (in_array($urlParts, self::REDIR_SUFFIXES)) {
                 $url = substr($url, 0, stripos($url, $urlParts));
@@ -45,18 +69,24 @@ class Page extends PageCommon {
                 header("Location: " . $url);
                 exit();
             } elseif (preg_match('/object([0-9]+)\.html/i', $urlParts, $regs)) {
-                return $this->getPageObject(intval($regs[1]));
+                return $this->getPageObject((int) $regs[1]);
             } elseif (preg_match('/([a-z0-9_-]+)\.html/i', $urlParts, $regs)) {
                 return $this->getPageObjectBySlug($regs[1]);
             } else {
                 return $this->getPageCity($url);
             }
         } else {
-            return FALSE;
+            return false;
         }
     }
 
-    private function getPageObjectBySlug($slugline) {
+    /**
+     * @param string $slugline
+     *
+     * @return string
+     */
+    private function getPageObjectBySlug($slugline)
+    {
         if (!$slugline) {
             return false;
         }
@@ -80,18 +110,35 @@ class Page extends PageCommon {
         $city = $pcs->getItemByPk($object['pt_citypage_id']);
 
         $shortDescr = strip_tags($object['pt_description']);
-        $short = mb_strlen($shortDescr) >= self::DESCRIPTION_TRESHOLD ? mb_substr($shortDescr, 0, mb_strpos($shortDescr, ' ', self::DESCRIPTION_TRESHOLD), 'utf-8') : $shortDescr;
+        $short = mb_strlen($shortDescr) >= self::DESCRIPTION_TRESHOLD ? mb_substr(
+            $shortDescr,
+            0,
+            mb_strpos(
+                $shortDescr,
+                ' ',
+                self::DESCRIPTION_TRESHOLD
+            ),
+            'utf-8'
+        ) : $shortDescr;
         $object['esc_name'] = htmlentities($object['pt_name'], ENT_QUOTES, 'utf-8');
         $object['map_zoom'] = ($object['pt_latlon_zoom']) ? $object['pt_latlon_zoom'] : 14;
         if ($object['pt_latitude'] && $object['pt_longitude']) {
             $object_lat_short = mb_substr($object['pt_latitude'], 0, 8);
             $object_lon_short = mb_substr($object['pt_longitude'], 0, 8);
-            $object['gps_dec'] = (($object_lat_short >= 0) ? 'N' : 'S') . abs($object_lat_short) . ' ' . (($object_lon_short >= 0) ? 'E' : 'W') . abs($object_lon_short);
+            $object['gps_dec'] = (($object_lat_short >= 0) ? 'N' : 'S') . abs(
+                    $object_lat_short
+                ) . ' ' . (($object_lon_short >= 0) ? 'E' : 'W') . abs($object_lon_short);
             $object['sw_ne_delta'] = 0.01;
-            $object['sw_ne'] = array(
-                'sw' => array('lat' => $object['pt_latitude'] - $object['sw_ne_delta'], 'lon' => $object['pt_longitude'] - $object['sw_ne_delta']),
-                'ne' => array('lat' => $object['pt_latitude'] + $object['sw_ne_delta'], 'lon' => $object['pt_longitude'] + $object['sw_ne_delta']),
-            );
+            $object['sw_ne'] = [
+                'sw' => [
+                    'lat' => $object['pt_latitude'] - $object['sw_ne_delta'],
+                    'lon' => $object['pt_longitude'] - $object['sw_ne_delta']
+                ],
+                'ne' => [
+                    'lat' => $object['pt_latitude'] + $object['sw_ne_delta'],
+                    'lon' => $object['pt_longitude'] + $object['sw_ne_delta']
+                ],
+            ];
             //$object['gps_deg'] = 0;
         }
 
@@ -111,10 +158,13 @@ class Page extends PageCommon {
         $this->addDataLD('@type', 'Place');
         $this->addDataLD('name', $object['esc_name']);
         $this->addDataLD('description', $short);
-        $this->addDataLD('address', array(
-            '@type' => 'PostalAddress',
-            'addressLocality' => $city['pc_title_unique'],
-        ));
+        $this->addDataLD(
+            'address',
+            [
+                '@type' => 'PostalAddress',
+                'addressLocality' => $city['pc_title_unique'],
+            ]
+        );
 
         if ($object['tr_sight']) {
             $this->addDescription('Достопримечательности ' . $city['pc_inwheretext']);
@@ -125,11 +175,14 @@ class Page extends PageCommon {
             $this->addCustomMeta('place:location:latitude', $object['pt_latitude']);
             $this->addCustomMeta('place:location:longitude', $object['pt_longitude']);
 
-            $this->addDataLD('geo', array(
-                '@type' => 'GeoCoordinates',
-                'latitude' => $object['pt_latitude'],
-                'longitude' => $object['pt_longitude'],
-            ));
+            $this->addDataLD(
+                'geo',
+                [
+                    '@type' => 'GeoCoordinates',
+                    'latitude' => $object['pt_latitude'],
+                    'longitude' => $object['pt_longitude'],
+                ]
+            );
         }
         $this->addDescription("{$object['tp_short']} {$city['pc_inwheretext']}");
         $this->addDescription($object['esc_name']);
@@ -148,7 +201,10 @@ class Page extends PageCommon {
         if ($object['pt_photo_id'] != 0) {
             $ph = new MPhotos($this->db);
             $photo = $ph->getItemByPk($object['pt_photo_id']);
-            $objImage = substr($photo['ph_src'], 0, 1) == '/' ? rtrim(_SITE_URL, '/') . $photo['ph_src'] : $photo['ph_src'];
+            $objImage = substr($photo['ph_src'], 0, 1) == '/' ? rtrim(
+                    _SITE_URL,
+                    '/'
+                ) . $photo['ph_src'] : $photo['ph_src'];
             $this->addOGMeta('image', $objImage);
         } else {
             $objImage = null;
@@ -176,7 +232,8 @@ class Page extends PageCommon {
         return $this->smarty->fetch(_DIR_TEMPLATES . '/_pages/pagepoint.sm.html');
     }
 
-    public function getSubContent($pid, $p_url) {
+    public function getSubContent($pid, $p_url)
+    {
         $dbm = $this->db->getTableName('modules');
         $this->db->sql = "SELECT md_url, md_title, md_keywords, md_description, md_pagecontent
                             FROM $dbm WHERE md_active = '1' AND md_pid = '$pid'";
@@ -197,7 +254,8 @@ class Page extends PageCommon {
         $this->getError('404');
     }
 
-    public function getNavigation($module_id, $sub_url) {
+    public function getNavigation($module_id, $sub_url)
+    {
         $dbm = $this->db->getTableName('modules');
         $this->db->sql = "SELECT md_title, md_url FROM $dbm WHERE md_active = '1' AND md_id = '$module_id' LIMIT 1";
         $this->db->exec();
@@ -206,9 +264,10 @@ class Page extends PageCommon {
         $res = $this->db->exec();
         if ($res) {
             while ($row = $this->db->fetch($res)) {
-                $navi = array('url' => $row['md_url'], 'title' => $row['md_title'], 'active' => false);
-                if ($row['md_url'] == $sub_url)
+                $navi = ['url' => $row['md_url'], 'title' => $row['md_title'], 'active' => false];
+                if ($row['md_url'] == $sub_url) {
                     $navi['active'] = true;
+                }
                 $navi_items[] = $navi;
             }
             if (isset($navi_items) && !empty($navi_items)) {
@@ -221,12 +280,16 @@ class Page extends PageCommon {
         }
     }
 
-    private function getPageCity($url) {
+    private function getPageCity($url)
+    {
         $url_parts = explode('/', $url);
-        $urlFiltered = array_map(function ($uItem) {
-            return trim(str_replace('+', ' ', $uItem));
-        }, $url_parts);
-        $urlFiltered = '/'.implode('/', array_filter($urlFiltered));
+        $urlFiltered = array_map(
+            function ($uItem) {
+                return trim(str_replace('+', ' ', $uItem));
+            },
+            $url_parts
+        );
+        $urlFiltered = '/' . implode('/', array_filter($urlFiltered));
         $lastPart = array_pop($url_parts);
         if ($lastPart == 'index.html') {
             header("HTTP/1.1 301 Moved Permanently");
@@ -285,12 +348,18 @@ class Page extends PageCommon {
             $this->addOGMeta('type', 'article');
             $this->addOGMeta('url', rtrim(_SITE_URL, '/') . $this->canonical);
             $this->addOGMeta('title', 'Достопримечательности ' . $row['pc_inwheretext']);
-            $this->addOGMeta('description', $row['pc_description'] . ($row['pc_announcement'] ? '. ' . $row['pc_announcement'] : ''));
+            $this->addOGMeta(
+                'description',
+                $row['pc_description'] . ($row['pc_announcement'] ? '. ' . $row['pc_announcement'] : '')
+            );
             $this->addOGMeta('updated_time', $this->lastedit_timestamp);
             if ($row['pc_coverphoto_id']) {
                 $ph = new MPhotos($this->db);
                 $photo = $ph->getItemByPk($row['pc_coverphoto_id']);
-                $cityImage = substr($photo['ph_src'], 0, 1) == '/' ? rtrim(_SITE_URL, '/') . $photo['ph_src'] : $photo['ph_src'];
+                $cityImage = substr($photo['ph_src'], 0, 1) == '/' ? rtrim(
+                        _SITE_URL,
+                        '/'
+                    ) . $photo['ph_src'] : $photo['ph_src'];
                 $this->addOGMeta('image', $cityImage);
             } else {
                 $cityImage = null;
@@ -303,7 +372,7 @@ class Page extends PageCommon {
             $this->smarty->assign('page_url', $this->basepath);
             $this->smarty->assign('page_image', $cityImage);
             $this->smarty->assign('types_select', $points_data['types']);
-            $this->smarty->assign('ptypes', array());
+            $this->smarty->assign('ptypes', []);
             $this->mainfile_js = _ER_REPORT ? ('../sys/static/?type=js&pack=city') : $this->globalsettings['res_js_city'];
 
             if ($this->checkEdit()) {
@@ -316,7 +385,11 @@ class Page extends PageCommon {
         }
     }
 
-    private function getPageObject($id) {
+    /**
+     * @param int $id
+     */
+    private function getPageObject($id)
+    {
         if (!$id) {
             return $this->getError('404');
         }
@@ -328,20 +401,38 @@ class Page extends PageCommon {
             return $this->getError('404');
         }
 
-        header("HTTP/1.1 301 Moved Permanently");
+        // фиксируем статистику старых адресов точек
+        $logFile = _DIR_DATA . '/logs/old_objects/' . $id . '.txt';
+        $ua = $_SERVER['HTTP_USER_AGENT'] ?? 'ua-undefined';
+        $referer = $_SERVER['HTTP_REFERER'] ?? 'referer-undefined';
+
+        $skip = false;
+        foreach (self::BOT_MARKERS as $marker) {
+            if (stripos($ua, $marker) !== false) {
+                $skip = true;
+            }
+        }
+
+        if (true || !$skip) {
+            $logEntry = date('Y-m-d H:i:s') . "\t" . $ua . "\t" . $referer . "\n";
+            file_put_contents($logFile, $logEntry, FILE_APPEND | LOCK_EX);
+        }
+
+        header('HTTP/1.1 301 Moved Permanently');
         header("Location: {$object['url_canonical']}");
         exit();
     }
 
-    private function getPageMap($url) {
+    private function getPageMap($url)
+    {
         $pc = new MPageCities($this->db);
         $city = $pc->getCityByUrl(str_replace('/map.html', '', $url));
         header("Location: /map/#center={$city['pc_longitude']},{$city['pc_latitude']}&zoom={$city['pc_latlon_zoom']}");
         exit();
     }
 
-    public static function getInstance($db, $mod = null) {
+    public static function getInstance($db, $mod = null)
+    {
         return self::getInstanceOf(__CLASS__, $db, $mod);
     }
-
 }
