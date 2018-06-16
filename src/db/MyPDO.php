f78e814f610a99ec568069ffb1d49ec33217e71f
Андрей Панисько <starkeen@gmail.com>
2018-06-16 14:24:45 +0300

    Убрал буферизацию запросов

--- src/db/MyPDO.php
+++ src/db/MyPDO.php
@@ -10,7 +10,6 @@ use PDOStatement;
 
 /**
  *
- * @author Andrey_Panisko
  */
 class MyPDO implements IDB
 {
@@ -56,9 +55,7 @@ class MyPDO implements IDB
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
                 PDO::ATTR_TIMEOUT => 600,
-                //PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
             ];
             $this->prefix = $db_prefix;
             try {
