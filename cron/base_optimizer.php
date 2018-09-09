e04e20355611a2d4282b7b3db0789a14932979a5
Андрей Панисько <starkeen@gmail.com>
2018-09-09 11:33:18 +0300

    Отключил оптимизацию таблиц

--- cron/base_optimizer.php
+++ cron/base_optimizer.php
@@ -15,16 +15,3 @@ $le->cleanExpired();
 
 $ni = new MNewsItems($db);
 $ni->cleanExpired();
-$ni->optimize();
-
-
-$cd = new MCityData($db);
-$cd->optimize();
-$pp = new MPagePoints($db);
-$pp->optimize();
-$pc = new MPageCities($db);
-$pc->optimize();
-$sp = new MSysProperties($db);
-$sp->optimize();
-$ws = new MWordstat($db);
-$ws->optimize();
