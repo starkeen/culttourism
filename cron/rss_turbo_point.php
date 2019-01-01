25a90e3b98d35f17c2f1d204b14072e65fcc8574
Андрей Панисько <starkeen@gmail.com>
2019-01-01 16:59:17 +0300

    Добавил генерацию по дням для RSS с точками

--- cron/rss_turbo_point.php
+++ cron/rss_turbo_point.php
@@ -3,10 +3,7 @@
 use app\model\criteria\PointCriteria;
 use app\rss\YandexTurboPointsGenerator;
 
-$fileName = sprintf('%s/feed/%s', _DIR_DATA, 'turbo-point.xml');
-
 $pointModel = new MPagePoints($db);
-
 $generator = new YandexTurboPointsGenerator($pointModel);
 
 $criteria = new PointCriteria();
@@ -15,5 +12,12 @@ $criteria->setLimit(1000);
 $criteria->addOrder('pt_rank', PointCriteria::ORDER_DESC);
 
 $xml = $generator->getXML($criteria);
+$fileName = sprintf('%s/feed/%s', _DIR_DATA, 'turbo-point.xml');
+file_put_contents($fileName, $xml->asXML());
 
+$dayToday = date('d');
+$dailyCriteria = clone $criteria;
+$dailyCriteria->addWhere('RIGHT(CAST(pt_id AS CHAR), 2) = ' . sprintf('%02d', $dayToday - 1));
+$xml = $generator->getXML($dailyCriteria);
+$fileName = sprintf('%s/feed/turbo-point-d%02d.xml', _DIR_DATA, $dayToday);
 file_put_contents($fileName, $xml->asXML());
