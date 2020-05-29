97041105c694a1577cd5e2ab5024fd56ee0398a9
Андрей Панисько <starkeen@gmail.com>
2020-05-29 08:40:50 +0300

    Исправил блок рекламы Яндекса

--- js/adv_city.js
+++ js/adv_city.js
@@ -1,28 +1,16 @@
 (function(w, d, n, s, t) {
     w[n] = w[n] || [];
     w[n].push(function() {
-        Ya.Direct.insertInto(94073, "yandex_ad", {
-            site_charset: "utf-8",
-            ad_format: "direct",
-            font_size: 1,
-            type: "horizontal",
-            border_type: "ad",
-            limit: 4,
-            title_font_size: 2,
-            site_bg_color: "FFFFFF",
-            header_bg_color: "FEEAC7",
-            border_color: "FBE5C0",
-            title_color: "0000CC",
-            url_color: "006600",
-            text_color: "000000",
-            hover_color: "0066FF",
-            favicon: true
+        Ya.Context.AdvManager.render({
+            blockId: "R-A-94073-3",
+            renderTo: "yandex_ad_city",
+            async: true
         });
     });
-    t = d.documentElement.firstChild;
+    t = d.getElementsByTagName("script")[0];
     s = d.createElement("script");
     s.type = "text/javascript";
-    s.src = "https://an.yandex.ru/system/context.js";
-    s.setAttribute("async", "true");
-    t.insertBefore(s, t.firstChild);
-})(window, document, "yandex_context_callbacks");
+    s.src = "//an.yandex.ru/system/context.js";
+    s.async = true;
+    t.parentNode.insertBefore(s, t);
+})(this, this.document, "yandexContextAsyncCallbacks");
