a6ef2ac569462e76d502338520ecf73b49e0f68e
Андрей Панисько <starkeen@gmail.com>
2020-06-19 19:33:26 +0300

    Добавил в логгер имплементацию верхнеуровневых методов

--- src/sys/Logger.php
+++ src/sys/Logger.php
@@ -5,54 +5,51 @@ declare(strict_types=1);
 namespace app\sys;
 
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 
 /**
  * Универсальный сервис отправки логов в контексте приложения
  */
 class Logger implements LoggerInterface
 {
-    public function __construct()
-    {
-    }
-
     public function emergency($message, array $context = []): void
     {
-        // TODO: Implement emergency() method.
+        $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
     public function alert($message, array $context = []): void
     {
-        // TODO: Implement alert() method.
+        $this->log(LogLevel::ALERT, $message, $context);
     }
 
     public function critical($message, array $context = []): void
     {
-        // TODO: Implement critical() method.
+        $this->log(LogLevel::CRITICAL, $message, $context);
     }
 
     public function error($message, array $context = []): void
     {
-        // TODO: Implement error() method.
+        $this->log(LogLevel::ERROR, $message, $context);
     }
 
     public function warning($message, array $context = []): void
     {
-        // TODO: Implement warning() method.
+        $this->log(LogLevel::WARNING, $message, $context);
     }
 
     public function notice($message, array $context = []): void
     {
-        // TODO: Implement notice() method.
+        $this->log(LogLevel::NOTICE, $message, $context);
     }
 
     public function info($message, array $context = []): void
     {
-        // TODO: Implement info() method.
+        $this->log(LogLevel::INFO, $message, $context);
     }
 
     public function debug($message, array $context = []): void
     {
-        // TODO: Implement debug() method.
+        $this->log(LogLevel::DEBUG, $message, $context);
     }
 
     public function log($level, $message, array $context = []): void
