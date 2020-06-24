755b14d3ed428e0e39b09d4d71bd96272859ae2e
Андрей Панисько <starkeen@gmail.com>
2020-06-24 11:08:06 +0300

    Исправил сериализацию контекста ошибок

--- src/sys/SentryLogger.php
+++ src/sys/SentryLogger.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace app\sys;
 
+use InvalidArgumentException;
 use Sentry\ClientBuilder;
 use Sentry\ClientInterface;
 use Sentry\SentrySdk;
@@ -50,13 +51,43 @@ class SentryLogger
      */
     public function captureMessage(Severity $level, string $message, array $context = []): ?string
     {
+        $plainContext = $this->plainContext($context);
+
         $hub = SentrySdk::getCurrentHub();
         $hub->configureScope(
-            static function (Scope $scope) use ($context): void {
-                $scope->setTags($context);
-                $scope->setExtras($context);
+            static function (Scope $scope) use ($plainContext): void {
+                $scope->setTags($plainContext);
+                $scope->setExtras($plainContext);
             }
         );
         return $hub->captureMessage($message, $level);
+    }
+
+    /**
+     * @param array $context
+     * @param string|null $prefix
+     *
+     * @return array
+     */
+    private function plainContext(array $context, string $prefix = null): array
+    {
+        $result = [];
+        foreach($context as $key => $value) {
+            $resultKey = $key;
+            if ($prefix !== null) {
+                $resultKey = $prefix . '_' . $resultKey;
+            }
+            if (is_scalar($value)) {
+                $result[$resultKey] = $value;
+            } elseif (is_array($value)) {
+                $result[$resultKey] = $this->plainContext($value, $key);
+            } elseif (is_object($value)) {
+                $result[$resultKey] = $this->plainContext((array) $value, $key);
+            } else {
+                throw new InvalidArgumentException('Недоступный для сериализации контекст');
+            }
+        }
+
+        return $result;
     }
 }
