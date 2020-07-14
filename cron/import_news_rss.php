71f32e6414f8c05d1c253a8ea61b92870817d5c8
Андрей Панисько <starkeen@gmail.com>
2020-07-14 17:27:22 +0300

    Перенёс импорт новостей в команду

--- cron/import_news_rss.php
+++ cron/import_news_rss.php
@@ -1,58 +1,9 @@
 <?php
 
+use app\crontab\ImportNewsCommand;
+
 $ni = new MNewsItems($db);
 $ns = new MNewsSources($db);
 
-$rss_tags = array(
-    'title',
-    'link',
-    'guid',
-    'comments',
-    'description',
-    'pubDate',
-    'category',
-);
-
-$sourses = $ns->getPortion();
-
-foreach ($sourses as $sourse) {
-    $rssfeed = rss_to_array('item', $rss_tags, $sourse['ns_url']);
-    foreach ($rssfeed as $item) {
-        $ni->add(array(
-            'source_id' => $sourse['ns_id'],
-            'pubdate' => date('Y-m-d H:i:s', strtotime($item['pubDate'])),
-            'title' => !empty($item['title']) ? $item['title'] : '[no title]',
-            'link' => $item['link'],
-            'description' => htmlentities($item['description'] ?? '', ENT_QUOTES, 'UTF-8'),
-        ));
-    }
-    $ns->updateByPk($sourse['ns_id'], array(
-        'ns_last_read' => $ns->now(),
-    ));
-}
-
-function rss_to_array($tag, $array, $url) {
-    $doc = new DOMdocument();
-    $rss_array = array();
-    $items = array();
-    try {
-        if (!@$doc->load($url)) {
-            throw new Exception("HTTP error [$url]");
-        }
-        foreach ($doc->getElementsByTagName($tag) AS $node) {
-            foreach ($array as $key => $value) {
-                $no = $node->getElementsByTagName($value);
-                if (isset($no) && is_object($no)) {
-                    if (is_object($no->item(0))) {
-                        $items[$value] = $no->item(0)->nodeValue;
-                    }
-                }
-            }
-            array_push($rss_array, $items);
-        }
-    } catch (Exception $e) {
-        echo 'Ошибка: ', $e->getMessage(), "\n";
-        //Logging::addHistory('cron', 'Ошибка получения новостей', $e->getMessage());
-    }
-    return $rss_array;
-}
+$command = new ImportNewsCommand($logger, $ni, $ns);
+$command->run();
---
+++ src/crontab/ImportNewsCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\crontab;
+
+use DOMDocument;
+use MNewsItems;
+use MNewsSources;
+use Psr\Log\LoggerInterface;
+use RuntimeException;
+use Throwable;
+
+class ImportNewsCommand extends CrontabCommand
+{
+    private const RSS_TAGS = [
+        'title',
+        'link',
+        'guid',
+        'comments',
+        'description',
+        'pubDate',
+        'category',
+    ];
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var MNewsItems
+     */
+    private $newsItemsModel;
+
+    /**
+     * @var MNewsSources
+     */
+    private $newsSourcesModel;
+
+    public function __construct(LoggerInterface $logger, MNewsItems $newsItemsModel, MNewsSources $newsSourcesModel)
+    {
+        $this->logger = $logger;
+        $this->newsItemsModel = $newsItemsModel;
+        $this->newsSourcesModel = $newsSourcesModel;
+    }
+
+    public function run(): void
+    {
+        $sources = $this->newsSourcesModel->getPortion();
+        foreach ($sources as $source) {
+            $rssFeed = $this->rssToArray('item', $source['ns_url']);
+            foreach ($rssFeed as $item) {
+                $this->newsItemsModel->add(
+                    [
+                        'source_id' => $source['ns_id'],
+                        'pubdate' => date('Y-m-d H:i:s', strtotime($item['pubDate'])),
+                        'title' => !empty($item['title']) ? $item['title'] : '[no title]',
+                        'link' => $item['link'],
+                        'description' => htmlentities($item['description'] ?? '', ENT_QUOTES, 'UTF-8'),
+                    ]
+                );
+            }
+            $this->newsSourcesModel->updateByPk(
+                $source['ns_id'],
+                [
+                    'ns_last_read' => $this->newsSourcesModel->now(),
+                ]
+            );
+        }
+    }
+
+    private function rssToArray($tag, $url): array
+    {
+        $doc = new DOMdocument();
+        $result = [];
+        try {
+            if (!$doc->load($url)) {
+                throw new RuntimeException("HTTP error [$url]");
+            }
+            foreach ($doc->getElementsByTagName($tag) as $node) {
+                $item = [];
+                foreach (self::RSS_TAGS as $value) {
+                    $no = $node->getElementsByTagName($value);
+                    if (isset($no) && is_object($no) && is_object($no->item(0))) {
+                        $item[$value] = (string) $no->item(0)->nodeValue;
+                    }
+                }
+                $result[] = $item;
+            }
+        } catch (Throwable $e) {
+            $this->logger->warning('Ошибка получения новостей', ['error' => $e->getMessage()]);
+        }
+
+        return $result;
+    }
+}
