370c7f653374a9be1df7c6c8dbbcd485db04769f
Андрей Панисько <starkeen@gmail.com>
2020-12-13 19:40:51 +0300

    Пометил некоторые методы используемыми

---
+++ .phpstorm.meta.php/common.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PHPSTORM_META {
+    override(\app\core\page\Content::getH1(), map(['' => '@']));
+    override(\app\core\page\Content::getUrlRss(), map(['' => '@']));
+    override(\app\core\page\Content::getUrlJs(), map(['' => '@']));
+    override(\app\core\page\Content::getUrlCss(), map(['' => '@']));
+}
