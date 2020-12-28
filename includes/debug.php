c1c0aa018b44987dd5a58ea662d86536bc6ca7d8
Андрей Панисько <starkeen@gmail.com>
2020-12-28 21:47:33 +0300

    Удалил отладочный код из composer

--- includes/debug.php
+++ includes/debug.php
@@ -1,40 +1,4 @@
 <?php
-function showSQL($sql) {
-    $colors = array('SELECT' => 'green', 'FROM' => 'green', 'WHERE' => 'green', 'AND' => 'green',
-            'ORDER BY' => 'blue', 'GROUP BY' => 'green',
-            'LIKE' => 'yellow', 'JOIN' => 'yellow', 'LEFT' => 'yellow',
-            'DESC' => 'magenta', 'LIMIT' => 'magenta', '%' => 'red');
-    $out = '<style>
-	#sqlback {
-	font-size:12px;
-	background: black;
-	color: white;
-	padding: 10px;
-	font-family: Courier New, Courier, monospace;
-	}
-	.green {
-	color:#00FF00;
-	}
-	.red {
-	color:#FB4F53;
-	}
-	.yellow {
-	color:#FFFF80;
-	}
-	.blue {
-	color:#80FFFF;
-	}
-	.magenta {
-	color:#FF80C0;
-	}
-	
-	</style>'."\n";
-
-    foreach ($colors as $word => $class) $sql = str_replace($word, "<span class='$class'>$word</span>", $sql);
-
-    $out .= "<div id='sqlback'>\n".nl2br($sql)."</div>\n";
-    echo $out;
-}
 
 function print_x($data) {
     if(is_array($data)) {
