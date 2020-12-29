cd610377f11ce076d99b64e3583c924ac0652ea1
Андрей Панисько <starkeen@gmail.com>
2020-12-29 17:08:47 +0300

    Расширил тест модуля City

--- tests/modules/CityModuleTest.php
+++ tests/modules/CityModuleTest.php
@@ -4,10 +4,37 @@ declare(strict_types=1);
 
 namespace tests\modules;
 
+use app\exceptions\NotFoundException;
 use app\modules\CityModule;
 
 class CityModuleTest extends AbstractModulesTestingDependencies
 {
+    public function testUndefinedRoute(): void
+    {
+        $module = $this->buildModule();
+
+        $request = $this->getMockRequest();
+        $response = $this->getMockResponse();
+        $request->expects(self::exactly(5))->method('getLevel1')->willReturn('undefined_route');
+
+        $this->expectException(NotFoundException::class);
+
+        $module->handle($request, $response);
+    }
+
+    public function testWeatherRouteWithoutLatLon(): void
+    {
+        $module = $this->buildModule();
+
+        $request = $this->getMockRequest();
+        $response = $this->getMockResponse();
+        $request->expects(self::exactly(5))->method('getLevel1')->willReturn('weather');
+
+        $this->expectException(NotFoundException::class);
+
+        $module->handle($request, $response);
+    }
+
     /**
      * @param string $key
      * @param bool $isApplicable
@@ -15,12 +42,7 @@ class CityModuleTest extends AbstractModulesTestingDependencies
      */
     public function testApplicableMethod(string $key, bool $isApplicable): void
     {
-        $db = $this->getMockDb();
-        $templateEngine = $this->getMockTemplateEngine();
-        $webUser = $this->getMockWebUser();
-        $globalConfig = $this->getMockGlobalConfig();
-
-        $module = new CityModule($db, $templateEngine, $webUser, $globalConfig);
+        $module = $this->buildModule();
 
         $request = $this->getMockRequest();
         $request->expects(self::once())->method('getModuleKey')->willReturn($key);
@@ -37,5 +59,18 @@ class CityModuleTest extends AbstractModulesTestingDependencies
             'подходящий запрос' => ['city', true],
             'неподходящий запрос' => ['other', false],
         ];
+    }
+
+    /**
+     * @return CityModule
+     */
+    private function buildModule(): CityModule
+    {
+        $db = $this->getMockDb();
+        $templateEngine = $this->getMockTemplateEngine();
+        $webUser = $this->getMockWebUser();
+        $globalConfig = $this->getMockGlobalConfig();
+
+        return new CityModule($db, $templateEngine, $webUser, $globalConfig);
     }
 }
