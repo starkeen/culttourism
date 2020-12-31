8934a4909b5cdc3f48a122e5e3738ca85bae9e4a
Андрей Панисько <starkeen@gmail.com>
2020-12-31 10:42:22 +0300

    Расширил тест

--- tests/services/openweathermap/WeatherServiceTest.php
+++ tests/services/openweathermap/WeatherServiceTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace tests\services\openweathermap;
 
 use app\services\openweathermap\HttpClientInterface;
+use app\services\openweathermap\WeatherData;
 use app\services\openweathermap\WeatherService;
 use PHPUnit\Framework\TestCase;
 
@@ -13,14 +14,17 @@ class WeatherServiceTest extends TestCase
     public function testRequest(): void
     {
         $http = $this->getMockBuilder(HttpClientInterface::class)->onlyMethods(['fetchData'])->getMock();
+        $resultMock = $this->getMockBuilder(WeatherData::class)->disableOriginalConstructor()->getMock();
 
         $service = new WeatherService($http, 'test_key_example');
 
         $http->expects(self::once())
             ->method('fetchData')
             ->with('http://api.openweathermap.org/data/2.5/weather?lat=12.34&lon=56.78&APPID=test_key_example&lang=ru&units=metric')
-            ->willReturn(null);
+            ->willReturn($resultMock);
 
-        $service->getWeatherByCoordinates(12.34, 56.78);
+        $result = $service->getWeatherByCoordinates(12.34, 56.78);
+
+        self::assertEquals($resultMock, $result);
     }
 }
