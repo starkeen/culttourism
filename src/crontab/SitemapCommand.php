62bc65d956fb931c8398bb4a8a4fe8392bcb3ad7
Андрей Панисько <starkeen@gmail.com>
2021-10-08 13:34:06 +0300

    Актуальная дата изменений в модулях

--- src/crontab/SitemapCommand.php
+++ src/crontab/SitemapCommand.php
@@ -30,11 +30,12 @@ class SitemapCommand extends CrontabCommand
         $filesizeOld = filesize($filename);
 
         $baseUrl = 'https://' . GLOBAL_URL_ROOT;
-        $baseDate = date('Y-m-d\TH:i:s+00:00');
 
         $urls = [];
 
-        $this->db->sql = "SELECT md_url FROM modules WHERE md_active = '1' AND md_robots = 'index, follow'";
+        $this->db->sql = "SELECT md_url, DATE_FORMAT(md_lastedit, '%Y-%m-%dT%H:%i:%s+00:00') date_update
+                          FROM modules
+                          WHERE md_active = 1 AND md_robots = 'index, follow'";
         $this->db->exec();
         while ($row = $this->db->fetch()) {
             $url['uri'] = $row['md_url'];
@@ -42,13 +43,13 @@ class SitemapCommand extends CrontabCommand
             if ($row['md_url'] !== 'index.html') {
                 $url['full'] .= '/';
             }
-            $url['lastmod'] = $baseDate;
+            $url['lastmod'] = $row['date_update'];
             $url['freq'] = 'weekly';
             $url['priority'] = ($row['md_url'] !== 'index.html') ? '0.90' : '1.00';
             $urls[] = $url;
         }
 
-        $this->db->sql = "SELECT u.url, DATE_FORMAT(c.pc_lastup_date, '%Y-%m-%dT%H:%i:%s+00:00') dateup
+        $this->db->sql = "SELECT u.url, DATE_FORMAT(c.pc_lastup_date, '%Y-%m-%dT%H:%i:%s+00:00') date_update
             FROM region_url u
                 LEFT JOIN pagecity c ON c.pc_id = u.citypage
             WHERE c.pc_text IS NOT NULL
@@ -57,14 +58,14 @@ class SitemapCommand extends CrontabCommand
         while ($row = $this->db->fetch()) {
             $url['uri'] = $row['url'];
             $url['full'] = "$baseUrl{$row['url']}/";
-            $url['lastmod'] = $row['dateup'];
+            $url['lastmod'] = $row['date_update'];
             $url['freq'] = 'weekly';
             $url['priority'] = '0.80';
             $urls[] = $url;
         }
 
         $this->db->sql = "SELECT concat(u.url, '/', p.pt_slugline, '.html') url,
-                                 DATE_FORMAT(p.pt_lastup_date, '%Y-%m-%dT%H:%i:%s+00:00') dateup
+                                 DATE_FORMAT(p.pt_lastup_date, '%Y-%m-%dT%H:%i:%s+00:00') date_update
             FROM pagepoints p
                 LEFT JOIN pagecity c ON c.pc_id = p.pt_citypage_id
                     LEFT JOIN region_url u ON u.uid = c.pc_url_id
@@ -74,7 +75,7 @@ class SitemapCommand extends CrontabCommand
         while ($row = $this->db->fetch()) {
             $url['uri'] = $row['url'];
             $url['full'] = "$baseUrl{$row['url']}";
-            $url['lastmod'] = $row['dateup'];
+            $url['lastmod'] = $row['date_update'];
             $url['freq'] = 'monthly';
             $url['priority'] = '0.70';
             $urls[] = $url;
@@ -102,7 +103,7 @@ class SitemapCommand extends CrontabCommand
     private function notifyGoogle(): void
     {
         $ch = curl_init();
-        $url = 'http://www.google.com/webmasters/sitemaps/ping?sitemap=http://culttourism.ru/sitemap.xml';
+        $url = 'http://www.google.com/webmasters/sitemaps/ping?sitemap=https://culttourism.ru/sitemap.xml';
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_exec($ch);
