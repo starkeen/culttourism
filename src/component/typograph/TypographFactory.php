c75515ec82577857444c5ff65ae3de68927beb3a
Андрей Панисько <starkeen@gmail.com>
2024-01-03 23:21:54 +0600

    Исправио типограф

--- src/component/typograph/TypographFactory.php
+++ src/component/typograph/TypographFactory.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace app\component\typograph;
 
 use JoliTypo\Fixer;
+use JoliTypo\Fixer\Dash;
 
 class TypographFactory
 {
     public static function build(): Typograph
     {
-        $fixer = new Fixer([]);
+        $fixer = new Fixer([Dash::class]);
 
         return new Typograph($fixer);
     }
