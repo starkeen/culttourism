6ac7d575df778c338c04dd536aeec4cfcd9c090f
Андрей Панисько <starkeen@gmail.com>
2024-06-11 11:15:00 +0500

    Исправил импорт Smarty

--- cron/wordstat_positions.php
+++ cron/wordstat_positions.php
@@ -4,6 +4,14 @@ use app\crontab\WordstatPositionsCommand;
 use GuzzleHttp\Client;
 use YandexSearchAPI\YandexSearchService;
 
+$currentTime = date('H:i');
+
+// Проверяем, попадает ли текущее время в диапазон от 00:00 до 08:00
+if ($currentTime >= '00:00' && $currentTime < '08:00') {
+    // Завершаем выполнение скрипта, экономим бюджет Яндекс Search API
+    exit();
+}
+
 $ws = new MWordstat($db);
 $searchService = new YandexSearchService(new Client(), $logger);
 $searchService->setApiId(YANDEX_SEARCH_ID);
